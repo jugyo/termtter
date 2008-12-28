4fdff1076868caf9be5608a28e91fd218c411db6
jugyo <kkohno@gmail.com>
2008-12-28 23:51:24 +0900

    notify-send 時のテキストのフォーマットをちょっとだけ修正

--- lib/termtter/notify-send.rb
+++ lib/termtter/notify-send.rb
@@ -5,7 +5,7 @@ Termtter::Client.add_hook do |statuses, event|
     text = statuses[0..(max - 1)].map{|s|
       status_text = s.text.gsub(%r{https?://[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+},'<a href="\0">\0</a>')
       status_text.gsub!("\n", '')
-      "<b>#{s.user_screen_name}</b> <span font=\"9.0\">#{status_text}</span>"
+      "<b>#{s.user_screen_name}:</b> <span font=\"9.0\">#{status_text}</span>"
     }.join("\n")
     
     text += "\n<a href=\"http://twitter.com/\">more…</a>" if statuses.size > max
