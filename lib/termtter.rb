efd5c12cb34beb5bff2d72657f7e55aa64fb9f18
jugyo <kkohno@gmail.com>
2008-12-28 23:16:38 +0900

    version up

--- lib/termtter.rb
+++ lib/termtter.rb
@@ -10,7 +10,7 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Termtter
-  VERSION = '0.2.1'
+  VERSION = '0.2.2'
   
   class Client
     
