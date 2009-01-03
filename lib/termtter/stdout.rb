bafcc94af7f0502c8cbd4e20ad0bbec150a493e2
jugyo <kkohno@gmail.com>
2009-01-04 02:02:08 +0900

    ソース整理

--- lib/termtter/stdout.rb
+++ lib/termtter/stdout.rb
@@ -13,7 +13,7 @@ Termtter::Client.add_hook do |statuses, event|
         text = s.text.gsub("\n", '')
         color_num = colors[s.user_screen_name.hash % colors.size]
         status = "#{s.user_screen_name}: #{text}"
-        unless s.in_reply_to_status_id.nil?
+        if s.in_reply_to_status_id
           status += " (reply to #{s.in_reply_to_status_id})"
         end
 
