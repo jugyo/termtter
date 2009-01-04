e6389f3495faf30cd49ebb9384f548a0bfb2b7b1
ujihisa <ujihisa@gmail.com>
2009-01-05 03:41:54 +0900

    import say.rb

---
+++ lib/termtter/say.rb
@@ -0,0 +1,22 @@
+raise 'say.rb runs only in OSX Leopard' if /darwin9/ !~ RUBY_PLATFORM
+
+# say :: String -> String -> IO ()
+def say(who, what)
+  voices = %w(Alex Bruce Fred Junior Ralph Agnes Kathy Princess Vicki Victoria Albert Bad\ News Bahh Bells Boing Bubbles Cellos Deranged Good\ News Hysterical Pipe\ Organ Trinoids Whisper Zarvox)
+  voice = voices[who.hash % voices.size]
+  system 'say', '-v', voice, what
+end
+
+class Termtter::Client
+  add_hook do |statuses, event, t|
+    if !statuses.empty? && event == :update_friends_timeline
+      statuses.reverse.each do |s|
+        text_without_uri = s.text.gsub(%r|https?://[^\s]+|, 'U.R.I.')
+        say s.user_screen_name, text_without_uri
+      end
+    end
+  end
+end
+
+# KNOWN BUG:
+# * exit or <C-c> doen't work quickly.
