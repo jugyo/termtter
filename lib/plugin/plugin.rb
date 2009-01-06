391def82e4c5f17dfec1b8900744e5524e5183dd
Tomohiro Nishimura <tomohiro68@gmail.com>
2009-01-07 00:28:11 +0900

    add plugins command and completion to plugin plugin

--- lib/plugin/plugin.rb
+++ lib/plugin/plugin.rb
@@ -1,6 +1,31 @@
 module Termtter::Client
+  public_storage[:plugins] = Dir["#{File.dirname(__FILE__)}/*.rb"].map do |f|
+    f.match(%r|([^/]+).rb$|)[1]
+  end
+
   add_command /^plugin\s+(.*)/ do |m, t|
     plugin m[1]
+  end
+
+  add_command /^plugins$/ do |m, t|
+    puts public_storage[:plugins].join("\n")
+  end
+
+  def self.find_plugin_candidates(a, b)
+    if a.empty?
+      public_storage[:plugins].to_a
+    else
+      public_storage[:plugins].grep(/^#{Regexp.quote a}/i)
+    end.
+    map {|u| b % u }
+  end
+
+  add_completion do |input|
+    if input =~ /^(plugin)\s+(.*)/
+      find_plugin_candidates $2, "#{$1} %s"
+    else
+      %w[ plugin plugins ].grep(/^#{Regexp.quote input}/)
+    end
   end
 end
 
