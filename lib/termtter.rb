0988053f8629d5af1709d788c06c32978ac0db58
ujihisa <ujihisa@gmail.com>
2009-01-06 20:20:42 +0900

    add warning and refactor

--- lib/termtter.rb
+++ lib/termtter.rb
@@ -28,10 +28,10 @@ alias original_require require
 def require(s)
   if %r|^termtter/(.*)| =~ s
     puts "[WARNING] use plugin '#{$1}' instead of require"
-    original_require "plugin/#{$1}"
-  else
-    original_require s
+    puts "  Such a legacy .termtter file will not be supported until version 1.0.0"
+    s = "plugin/#{$1}"
   end
+  original_require s
 end
 
 module Termtter
