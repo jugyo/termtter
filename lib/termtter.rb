44ad788a5674631235d76c83258d5c6017236cf7
jugyo <kkohno@gmail.com>
2009-01-06 14:00:14 +0900

    Readline.basic_word_break_characters はデフォルト値を使用するようにした

--- lib/termtter.rb
+++ lib/termtter.rb
@@ -126,7 +126,6 @@ module Termtter
         @@completions.clear
       end
 
-      Readline.basic_word_break_characters= "\t\n\"\\'`><=;|&{("
       Readline.completion_proc = proc {|input|
         @@completions.map {|completion| completion.call(input)}.flatten
       }
