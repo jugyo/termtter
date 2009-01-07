f172105174e1bf557c368be9caca19ef28431c0b
ujihisa <ujihisa@gmail.com>
2009-01-08 02:11:22 +0900

    add spam plugin

---
+++ lib/plugin/spam.rb
@@ -0,0 +1,6 @@
+Termtter::Twitter.new(configatron.user_name, configatron.password).update_status('*super spam time*')
+module Termtter::Client
+  add_command /.+/ do |m, t|
+    Thread.new { t.update_status(m[0]) }
+  end
+end
