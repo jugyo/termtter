af7f54ed651c97475354f5377c8555b4dde7f6f2
bubblegum <takaakisone@gmail.com>
2009-01-08 00:14:37 +0900

    update時に[Y/n]で確認。誤入力防止。

---
+++ lib/plugin/yesno.rb
@@ -0,0 +1,25 @@
+module Termtter::Client
+
+  add_command /^yesno\s+(.*)/ do |m, t|
+    text = m[1]
+    unless text.empty?
+      pause
+      print "update? #{text} [Y/n] "
+      buf = Readline.readline("", false)
+      t.update_status(text) if buf =~ /^y?$/i
+      resume
+    end
+  end
+
+  add_completion do |input|
+    case input
+    when /^(yesno)\s+(.*)@([^\s]*)$/
+      find_user_candidates $3, "#{$1} #{$2}@%s"
+    else
+      ['yesno'].grep(/^#{Regexp.quote input}/)
+    end
+  end
+
+end
+
+# FIXME!!! command name
