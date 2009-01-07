70696241a3b735e4886fb0ac4074b419ad0aa2a3
Tomohiro Nishimura <tomohiro68@gmail.com>
2009-01-07 17:21:56 +0900

    git rm plugin/filter-* and mv filter-english.rb

--- lib/filter/english.rb
+++ lib/filter/english.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 Termtter::Client.add_filter do |statuses|
-  statuses.select {|s| s.english? }
+  statuses.select &:english?
 end
 
 # filter-english.rb
--- lib/plugin/filter-en2ja.rb
+++
@@ -1,9 +0,0 @@
-plugin 'translation'
-
-Termtter::Client.add_filter do |statuses|
-  statuses.each do |s|
-    if s.english?
-      s.text = transelate(s.text, 'en|ja')
-    end
-  end
-end
