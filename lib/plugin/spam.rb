8725fa0be3065c4b2cd2eb7e61d9afd1744cbb25
ujihisa <ujihisa@gmail.com>
2009-01-08 02:18:03 +0900

    fix bug

--- lib/plugin/spam.rb
+++ lib/plugin/spam.rb
@@ -1,5 +1,6 @@
 Termtter::Twitter.new(configatron.user_name, configatron.password).update_status('*super spam time*')
 module Termtter::Client
+  clear_commands
   add_command /.+/ do |m, t|
     Thread.new { t.update_status(m[0]) }
   end
