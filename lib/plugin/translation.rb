e9d092dadaf2fdd3c00b99d48f69959359f2ecc6
jugyo <kkohno@gmail.com>
2009-01-07 15:09:11 +0900

    翻訳機能だけを取り出してファイル名も変えた

---
+++ lib/plugin/translation.rb
@@ -0,0 +1,37 @@
+require 'nokogiri'
+require 'net/http'
+require 'kconv'
+require 'uri'
+
+#plugin 'english'
+
+module Termtter
+  class Status
+    def english?
+      @text !~ /[一-龠]+|[ぁ-ん]+|[ァ-ヴー]+|[ａ-ｚＡ-Ｚ０-９]+/
+    end
+  end
+end
+
+def transelate(text, langpair)
+  req = Net::HTTP::Post.new('/translate_t')
+  req.add_field('Content-Type', 'application/x-www-form-urlencoded')
+  req.add_field('User-Agent', 'Mozilla/5.0')
+  Net::HTTP.version_1_2 # Proxy に対応してない
+  Net::HTTP.start('translate.google.co.jp', 80) {|http|
+    response = http.request(req, "langpair=#{langpair}&text=#{URI.escape(text)}")
+    open('en2ja_test.html', 'w'){|f| f << response.body}
+    doc = Nokogiri::HTML.parse(response.body, nil, 'utf-8')
+    return doc.css('#result_box').text
+  }
+end
+
+Termtter::Client.add_command /^(en2ja|ja2en)\s+(.*)$/ do |m, t|
+  langpair = m[1].gsub('2', '|')
+  puts "translating..."
+  puts "=> #{transelate(m[2], langpair)}"
+end
+
+# This plugin does not work yet.
+# requirements
+#   nokogiri (sudo gem install nokogiri)
