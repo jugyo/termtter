365a407d91d237edc9c648d9fe81c3216e183d65
hitode909 <hitode909@gmail.com>
2009-01-07 11:02:42 +0900

    Made filter. Added some files and modified termtter.rb.

---
+++ lib/plugin/filter-reverse.rb
@@ -0,0 +1,11 @@
+module Termtter::Client
+  add_filter do |statuses|
+    statuses.map do |s|
+      s.text = s.text.split(//).reverse.to_s
+      s
+    end
+  end
+end
+
+# filter-reverse.rb
+#   reverse texts
