f386ce814b370fb170ec4059631c45b3e469f5a1
mattn <mattn.jp@gmail.com>
2009-01-08 12:41:48 +0900

    fixed nuance in help of follow/leave command.

--- lib/plugin/follow.rb
+++ lib/plugin/follow.rb
@@ -1,23 +1,23 @@
 module Termtter::Client
-  add_help 'follow @USER', 'Follow user'
+  add_help 'follow USERNAME', 'Follow user'
 
   add_command %r'^follow\s+(\w+)$' do |m, t|
     user = m[1]
     res = t.follow(user)
     if res.code == '200'
-      puts "Followed user @#{user}"
+      puts "Followed user #{user}"
     else
       puts "Failed: #{res}"
     end
   end
 
-  add_help 'leave @USER', 'Leave user'
+  add_help 'leave USERNAME', 'Leave user'
 
   add_command %r'^leave\s+(\w+)$' do |m, t|
     user = m[1]
     res = t.leave(user)
     if res.code == '200'
-      puts "Leaved user @#{user}"
+      puts "Leaved user #{user}"
     else
       puts "Failed: #{res}"
     end
