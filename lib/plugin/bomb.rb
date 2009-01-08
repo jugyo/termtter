9823b9f4403427246433a72dfa969aea69fbab1d
hakobe932 <hakobe@gmail.com>
2009-01-08 18:05:22 +0900

    爆発させれるようにした

--- lib/plugin/bomb.rb
+++ lib/plugin/bomb.rb
@@ -4,11 +4,23 @@ module Termtter
       self.text =~ /爆発/
     end
   end
+
+  module Client
+    add_help 'bomb WORD', 'Bomb it'
+    add_command %r'^bomb\s+(.+)$' do |m, t|
+      bomb = m[1]
+      msg = "#{bomb} 爆発しろ!"
+
+      puts msg
+      t.update_status msg
+    end
+  end
 end
 
 # bomb.rb
-# See http://gyazo.com/4b33517380673d92f51a52e675ecdb02.png .
+# Bomb it!
 #
+# See http://gyazo.com/4b33517380673d92f51a52e675ecdb02.png .
 # configatron.plugins.stdout.timeline_format =
 #  '<%= color(time, 90) %> <%= s.bomb? ? color(color(status, 41), 37) : color(status, status_color) %> <%= color(id, 90) %>'
 
