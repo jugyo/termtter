10042a00aeb86303571358cae97d87827937f6bc
hitode909 <hitode909@gmail.com>
2009-01-09 00:15:20 +0900

    made history plugin(save timeline to file)

--- lib/plugin/history.rb
+++ lib/plugin/history.rb
@@ -1,13 +1,46 @@
-configatron.plugins.history.set_default('filename',
-                        '~/.termtter_history')
+require 'yaml'
 
-# undef constructing
-Termtter::Client.add_hook do |statuses, event|
-  case event
-  when :initialize
-    puts "will load from #{configatron.plugins.history.filename}"
-  when :exit
-    puts "will save to #{configatron.plugins.history.filename}"
+configatron.plugins.history.set_default('filename',
+                                        '~/.termtter_history')
+configatron.plugins.history.set_default('keys',
+                                        [:log, :users, :status_ids])
+
+module Termtter::Client
+  def self.load_history
+    filename = File.expand_path(configatron.plugins.history.filename)
+    keys = configatron.plugins.history.keys
+
+    if File.exist?(filename)
+      history = YAML.load_file(filename)
+      keys.each do |key|
+        public_storage[key] = history[key]
+      end
+      puts "history loaded(#{File.size(filename)}bytes)"
+    end
+  end
+
+  def self.save_history
+    filename = File.expand_path(configatron.plugins.history.filename)
+    keys = configatron.plugins.history.keys
+    history = { }
+    keys.each do |key|
+      history[key] = public_storage[key]
+    end
+
+    YAML.dump( history, File.open(filename, 'w') )
+    
+    puts "history saved(#{File.size(filename)}bytes)"
+  end
+
+  add_hook do |statuses, event|
+    case event
+    when :initialize
+      load_history
+    when :exit
+      save_history
+    end
   end
 end
 
+# history.rb
+#   save log to file
