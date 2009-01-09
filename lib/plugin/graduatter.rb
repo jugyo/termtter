84daea19460a19917951b25113b7366f0e843646
ujihisa <ujihisa@gmail.com>
2009-01-09 20:47:27 +0900

    threaded

--- lib/plugin/graduatter.rb
+++ lib/plugin/graduatter.rb
@@ -1,3 +1,6 @@
 100.times do
-  Termtter::Twitter.new(configatron.user_name, configatron.password).update_status("I decided not to use twitter so as not to leave university before I complete the dissertation.")
+  Thread.start do
+    Termtter::Twitter.new(configatron.user_name, configatron.password).update_status("I decided not to use twitter so as not to leave university before I complete the dissertation.")
+  end
+  sleep 5
 end
