9ceb9cae3fbfa166b91773c2f8b2efa9f72a8bd8
Tomohiro Nishimura <tomohiro68@gmail.com>
2009-01-09 12:35:34 +0900

    add ls and cd command to sl plugin

--- lib/plugin/sl.rb
+++ lib/plugin/sl.rb
@@ -1,3 +1,24 @@
 module Termtter::Client
+  public_storage[:current] = ''
+
   add_macro /^sl$/, 'eval system "sl"'
+
+  add_help 'ls', 'Show current directory'
+  add_command /^ls$/ do |m, t|
+    call_commands "list #{public_storage[:current]}", t
+  end
+
+  add_help 'cd USER', 'Change current directory'
+  add_command /^cd\s+(.*)/ do |m, t|
+    public_storage[:current] = m[1].strip
+  end
+
+  add_completion do |input|
+    case input
+    when /^(cd)\s+(.*)/
+      find_user_candidates $2, "#{$1} %s"
+    else
+      %w[ sl ls cd ].grep(/^#{Regexp.quote input}/)
+    end
+  end
 end
