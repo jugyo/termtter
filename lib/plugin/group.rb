52c32352d4e957f24bab57f7e989372dbefa1ff6
hitode909 <hitode909@gmail.com>
2009-01-09 17:39:26 +0900

    improved print format in some commands

--- lib/plugin/group.rb
+++ lib/plugin/group.rb
@@ -16,7 +16,7 @@ module Termtter::Client
       statuses = group ? public_storage[:log].select { |s|
         group.include?(s.user_screen_name) 
       } : []
-      call_hooks(statuses, :list_friends_timeline, t)
+      call_hooks(statuses, :search, t)
     end
 
     def self.find_group_candidates(a, b)
