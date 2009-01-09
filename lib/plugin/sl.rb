c9f7bb78af84fb2fbf6cb88703baf8e696e66751
Tomohiro Nishimura <tomohiro68@gmail.com>
2009-01-09 12:43:37 +0900

    add pwd command to sl plugin

--- lib/plugin/sl.rb
+++ lib/plugin/sl.rb
@@ -1,10 +1,12 @@
 module Termtter::Client
   public_storage[:current] = ''
 
-  add_macro /^sl$/, 'eval system "sl"'
+  add_macro /^sl\s*$/, 'eval system "sl"'
+
+  add_macro /^pwd\s*$/, 'eval public_storage[:current]'
 
   add_help 'ls', 'Show current directory'
-  add_command /^ls$/ do |m, t|
+  add_command /^ls\s*$/ do |m, t|
     call_commands "list #{public_storage[:current]}", t
   end
 
@@ -18,7 +20,7 @@ module Termtter::Client
     when /^(cd)\s+(.*)/
       find_user_candidates $2, "#{$1} %s"
     else
-      %w[ sl ls cd ].grep(/^#{Regexp.quote input}/)
+      %w[ sl ls cd pwd ].grep(/^#{Regexp.quote input}/)
     end
   end
 end
