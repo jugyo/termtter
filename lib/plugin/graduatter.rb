7d3c4735ce24f1a6fb06e0bb4e3e5ffccd2d87db
ujihisa <ujihisa@gmail.com>
2009-01-09 21:41:02 +0900

    refactoring

--- lib/plugin/graduatter.rb
+++ lib/plugin/graduatter.rb
@@ -1,7 +1,7 @@
 twitter = Termtter::Twitter.new(configatron.user_name, configatron.password)
 Thread.start do
   100.times do |i|
-    flg = i % 2 == 0
-    twitter.update_status("I decided not to use twitter so as not to leave university before I complete the dissertation#{flg ? '!' : '.'}")
+    twitter.update_status(
+      "I decided not to use twitter so as not to leave university before I complete the dissertation#{i.odd? ? '!' : '.'}")
   end
 end
