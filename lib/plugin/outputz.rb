dc4e537c53eeba2dd75fe97d94fa9db9c60b1089
Tomohiro Nishimura <tomohiro68@gmail.com>
2009-01-10 17:26:18 +0900

    fix outputz bug

--- lib/plugin/outputz.rb
+++ lib/plugin/outputz.rb
@@ -6,29 +6,28 @@ module Termtter::Client
   if key.instance_of? Configatron::Store
     puts 'Need your secret key'
     puts 'please set configatron.plugins.outputz.secret_key'
-    return false
-  end
-
-  add_command /^(update|u)\s+(.*)/ do |m, t|
-    text = ERB.new(m[2]).result(binding).gsub(/\n/, ' ')
-    unless text.empty?
-      t.update_status(text)
-      puts "=> #{text}"
-    end
-    t.instance_variable_get('@connection').
-      start('outputz.com', 80) do |http|
-        key  = CGI.escape key
-        uri  = CGI.escape configatron.plugins.outputz.uri
-        size = text.split(//).size
-        http.post('/api/post', "key=#{key}&uri=#{uri}&size=#{size}")
+  else
+    add_command /^(update|u)\s+(.*)/ do |m, t|
+      text = ERB.new(m[2]).result(binding).gsub(/\n/, ' ')
+      unless text.empty?
+        t.update_status(text)
+        puts "=> #{text}"
       end
+      t.instance_variable_get('@connection').
+        start('outputz.com', 80) do |http|
+          key  = CGI.escape key
+          uri  = CGI.escape configatron.plugins.outputz.uri
+          size = text.split(//).size
+          http.post('/api/post', "key=#{key}&uri=#{uri}&size=#{size}")
+        end
+    end
   end
 end
 
 # outputz.rb
 #   a plugin that report to outputz your post
 #
-# settings
-#   plugin 'outputz'
+# settings (note: must this order)
 #   configatron.plugins.outputz.secret_key = 'your secret key'
+#   plugin 'outputz'
 
