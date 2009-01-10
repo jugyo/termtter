42e40448f3436f92c0989828d3dffd6d90ec4f48
hitode909 <hitode909@gmail.com>
2009-01-11 00:11:59 +0900

    add update_editor plugin.

---
+++ lib/plugin/update_editor.rb
@@ -0,0 +1,42 @@
+require 'tempfile'
+
+module Termtter::Client
+  if ENV['EDITOR']
+    configatron.plugins.update_editor.set_default('editor', ENV['EDITOR'])
+  else
+    configatron.plugins.update_editor.set_default('editor', 'vi')
+  end
+
+  
+  def self.input_editor
+    file = Tempfile.new('termtter')
+    editor = configatron.plugins.update_editor.editor
+    system("#{editor} #{file.path}")
+    result = file.read
+    file.close
+    result
+  end
+
+  add_command /^(update_editor|ue)\s*$/ do |m, t|
+    pause
+    text = input_editor.gsub("\n", " ")
+    unless text.empty?
+      t.update_status(text)
+      puts "=> #{text}"
+    end
+    resume
+  end
+
+  add_help 'update_editor,ue', 'Update status from editor.'
+
+  add_completion do |input|
+    %w[ update_editor ].grep(/^#{Regexp.quote input}/)
+  end
+end
+
+# update_editor.rb
+#   update status from editor.
+# example:
+#   > update_editor
+#   (type your status, save, close the editor)
+#   => (your status)
