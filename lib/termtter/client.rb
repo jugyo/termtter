0cd92e91593d18addb1b0bd7eba05c32a9f38950
jugyo <kkohno@gmail.com>
2009-01-11 00:17:12 +0900

    設定読み込んだりする処理を termtter/client.rb に移動

--- lib/termtter/client.rb
+++ lib/termtter/client.rb
@@ -1,5 +1,6 @@
 module Termtter
   class CommandNotFound < StandardError; end
+
   module Client
 
     @@hooks = []
@@ -130,7 +131,51 @@ module Termtter
         @@input_thread.kill
       end
 
+      def load_default_plugins
+        plugin 'standard_plugins'
+        plugin 'stdout'
+      end
+
+      def load_config
+        conf_file = File.expand_path('~/.termtter')
+        if File.exist? conf_file
+          load conf_file
+        else
+          HighLine.track_eof = false
+          ui = HighLine.new
+          username = ui.ask('your twitter username: ')
+          password = ui.ask('your twitter password: ') { |q| q.echo = false }
+
+          File.open(File.expand_path('~/.termtter'), 'w') {|io|
+            plugins = Dir.glob(File.dirname(__FILE__) + "/../lib/plugin/*.rb").map  {|f|
+              f.match(%r|lib/plugin/(.*?).rb$|)[1]
+            }
+            plugins -= %w[stdout standard_plugins]
+            plugins.each do |p|
+              io.puts "#plugin '#{p}'"
+            end
+
+            io.puts
+            io.puts "configatron.user_name = '#{username}'"
+            io.puts "configatron.password = '#{password}'"
+            io.puts "#configatron.update_interval = 120"
+            io.puts "#configatron.proxy.host = 'proxy host'"
+            io.puts "#configatron.proxy.port = '8080'"
+            io.puts "#configatron.proxy.user_name = 'proxy user'"
+            io.puts "#configatron.proxy.password = 'proxy password'"
+            io.puts
+            io.puts "# vim: set filetype=ruby"
+          }
+          puts "generated: ~/.termtter"
+          puts "enjoy!"
+          load conf_file
+        end
+      end
+
       def run
+        load_default_plugins()
+        load_config()
+
         puts 'initializing...'
         initialized = false
         @@pause = false
