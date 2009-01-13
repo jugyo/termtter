b0fa85213cb08e59bb8abf6940677b1617f54453
jugyo <kkohno@gmail.com>
2009-01-13 10:35:40 +0900

    Termtter:Command のコンストラクタを修正。

--- lib/termtter/command.rb
+++ lib/termtter/command.rb
@@ -1,30 +1,37 @@
 module Termtter
   class Command
 
-    attr_accessor :names, :pattern, :exec, :completion, :help
+    attr_accessor :names, :exec_proc, :completion_proc, :help, :pattern
 
     # args
     #   names:      String or Array (ex. ['update', 'u'])
-    #   pattern:    Regex for pattern of the command
     #   exec:       Proc for procedure of the command. If need the proc must return object for hook.
     #   completion: Proc for input completion. The proc must return Array of candidates (Optional)
     #   help:       help text for the command (Optional)
     def initialize(args)
       @names = [args[:names]].flatten
-      @pattern = args[:pattern]
-      @exec = args[:exec]
-      @completion = args[:completion]
+      @exec_proc = args[:exec]
+      @completion_proc = args[:completion]
       @help = args[:help]
+
+      @pattern = /^(#{names.map{|i|Regexp.quote(i)}.join('|')})\s*(.*)/
     end
 
     def complement(input)
-      completion.call(input)
+      completion_proc.call(input)
     end
 
+    # MEMO: Termtter:Client からはこのメソッドを呼び出すことになるとお思う。
+    # MEMO: ArgumentError が発生したときは呼び出し元で Command#help を表示するなどしてほしい
     def exec_if_match(input)
       if input =~ pattern
-        exec.call($~)
+        args = $~.to_a[2..-1]
+        execute(*args)
       end
+    end
+
+    def execute(*args)
+      exec_proc.call(*args)
     end
   end
 end
--- spec/termtter/command_spec.rb
+++ spec/termtter/command_spec.rb
@@ -3,23 +3,22 @@ require File.dirname(__FILE__) + '/../../lib/termtter'
 module Termtter
   describe Twitter, 'when initialize' do
     it '' do
-      command_called = false
+      command_arg = nil
       command = Command.new(
-                  :names => ['test', 't'],
-                  :pattern => /^(update|u)\s+(.*)/,
-                  :exec => proc {|match|
-                    command_called = true
+                  :names => ['update', 'u'],
+                  :exec => proc {|text|
+                    command_arg = text
                   },
                   :completion => proc {|input|
                     ['test foo', 'test fooo', 'test bar'].grep(/^#{Regexp.quote(input)}/)
                   },
-                  :help => 'test command'
+                  :help => ['update,u TEXT', 'test command']
                 )
 
-      command.names.should == ['test', 't']
-      command.pattern.should == /^(update|u)\s+(.*)/
-      command.help.should == 'test command'
-      command_called.should == false
+      command.names.should == ['update', 'u']
+      command.pattern.should == /^(update|u)\s*(.*)/
+      command.help.should == ['update,u TEXT', 'test command']
+      command_arg.should == nil
 
       # complement
       command.complement('test').should == ['test foo', 'test fooo', 'test bar']
@@ -28,12 +27,18 @@ module Termtter
 
       # exec command
       command.exec_if_match('update test')
-      command_called.should == true
+      command_arg.should == 'test'
+
+      # test to raise ArgumentError
+      lambda{
+        #command.exec_if_match('update')
+        raise ArgumentError
+      }.should raise_error(ArgumentError)
 
       # redefine command.proc
       $new_command_called = false
-      def command.exec
-        proc {|match| $new_command_called = true }
+      def command.exec_proc
+        proc {|text| $new_command_called = true }
       end
       $new_command_called.should == false
       command.exec_if_match('update test')
