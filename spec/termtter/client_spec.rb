3acff21ff7a88cc1b5669c810de9061dea953076
jugyo <kkohno@gmail.com>
2009-01-14 23:08:49 +0900

    rename method

--- spec/termtter/client_spec.rb
+++ spec/termtter/client_spec.rb
@@ -4,14 +4,14 @@ module Termtter
   describe Client do
     it 'should take new_command' do
       command = Command.new(:name => :test)
-      Client.add_new_command(command)
-      Client.get_new_command(:test).should == command
+      Client.register_command(command)
+      Client.get_command(:test).should == command
     end
 
     it 'should call new_command' do
       command_arg = nil
       command = Command.new(:name => :test, :exec_proc => proc {|arg| command_arg = arg || 'nil'})
-      Client.add_new_command(command)
+      Client.register_command(command)
 
       command_arg.should == nil
       Client.call_commands('test', nil)
