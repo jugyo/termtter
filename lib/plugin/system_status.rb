3845e06e573f71a45ca8ea0d8bd6148ee247c5eb
jugyo <kkohno@gmail.com>
2009-01-14 21:20:17 +0900

    refactoring

--- lib/plugin/system_status.rb
+++ lib/plugin/system_status.rb
@@ -1,17 +1,19 @@
 require 'erb'
 
 configatron.plugins.termtter_status.set_default(:interval, 1)
-configatron.plugins.termtter_status.set_default(:background, :on_blue)
+configatron.plugins.termtter_status.set_default(:default_color, :on_blue)
 configatron.plugins.termtter_status.set_default(:format, '<%= status %>')
 
 module Termtter::Client
   Thread.new do
     loop do
-      status = public_storage[:system_status] || Time.now.strftime("%x %X")
-      status = ERB.new(configatron.plugins.termtter_status.format).result(binding)
-      back = status.size - 1
-      colored_status = color(status, configatron.plugins.termtter_status.background)
-      print "\e[s\e[1000G\e[#{back}D#{colored_status}\e[u"
+      status = public_storage[:system_status] || 
+                  Time.now.strftime("%x %X")
+      formatted_status = ERB.new(configatron.plugins.termtter_status.format).result(binding)
+      color = public_storage[:system_status_color] || 
+                      configatron.plugins.termtter_status.default_color
+      colored_status = color(formatted_status, color)
+      print "\e[s\e[1000G\e[#{status.size - 1}D#{colored_status}\e[u"
       $stdout.flush
       sleep configatron.plugins.termtter_status.interval
     end
@@ -23,6 +25,6 @@ end
 #   output public_storage[:system_status] or Time.now.strftime("%x %X") if nil
 # example config
 #   configatron.plugins.termtter_status.interval = 1
-#   configatron.plugins.termtter_status.backgrond = :on_blue
-#   configatron.plugins.termtter_status.format = '<%= public_storage[:system_status] %>'
+#   configatron.plugins.termtter_status.default_color = :on_blue
+#   configatron.plugins.termtter_status.format = '<%= status %>'
 
