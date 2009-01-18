4353cf0dd2e4992e9308104251bbaccd9afbfea3
jugyo <kkohno@gmail.com>
2009-01-18 17:20:47 +0900

    create spec for find_status_id_candidates

--- spec/plugin/standard_plugins_spec.rb
+++ spec/plugin/standard_plugins_spec.rb
@@ -9,6 +9,12 @@ module Termtter
       command.name.should == :profile
       # TODO: more spec for commands
     end
+
+    it 'should return candidates when call find_status_id_candidates' do
+      Client.public_storage[:status_ids] = %w[1 2 22 3 4 5]
+      Client.find_status_id_candidates("1", "%s").should == ["1"]
+      Client.find_status_id_candidates("2", "%s").should == ["2", "22"]
+    end
   end
 end
 
