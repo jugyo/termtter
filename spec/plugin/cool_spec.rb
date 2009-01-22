363119e669ccc0db71e2aa7d0254a6f9089e5b07
jugyo <kkohno@gmail.com>
2009-01-22 22:23:49 +0900

    change to use register_macro

--- spec/plugin/cool_spec.rb
+++ spec/plugin/cool_spec.rb
@@ -2,9 +2,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Termtter::Client, 'when the plugin cool is loaded' do
   it 'should add something about cool' do
-    Termtter::Client.should_receive(:add_help)
-    Termtter::Client.should_receive(:add_macro)
-    Termtter::Client.should_receive(:add_completion)
+    Termtter::Client.should_receive(:register_macro)
     plugin 'cool'
   end
 end
