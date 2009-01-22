5032ddd92ba5f18abb571eea24e24db5f642c1a4
mattn <mattn.jp@gmail.com>
2009-01-22 19:36:40 +0900

    change to new type command registration.

--- lib/plugin/hatebu.rb
+++ lib/plugin/hatebu.rb
@@ -2,50 +2,50 @@ require 'rubygems'
 require 'atomutil'
 
 module Termtter::Client
-  add_help 'hatebu ID', 'Hatena bookmark a status'
-
-  add_command %r'^hatebu\s+(\d+)(.*)$' do |m, t|
-    id = m[1]
-    comment = m[2].strip
-    statuses = public_storage[:log].select { |s| s.id == id }
-    unless statuses.empty?
-      status = statuses.first
-    else
-      status = t.show(id).first
-    end
-    auth = auth = Atompub::Auth::Wsse.new({
-        :username => configatron.plugins.hatebu.username,
-        :password => configatron.plugins.hatebu.password,
-    })
-    link = Atom::Link.new({
-       :href => "http://twitter.com/#{status.user_screen_name}/status/#{status.id}",
-       :rel => 'related',
-       :type => 'text/html',
-    })
-    entry = Atom::Entry.new({
-        :link => link,
-        :title => 'dummy',
-        :summary => comment,
-    })
-    req = Net::HTTP::Post.new 'http://b.hatena.ne.jp/atom/post'
-    req['User-Agent'] = 'Mozilla/5.0'
-    req['Content-Type'] = 'application/atom+xml'
-    req['Slug'] = 'termtter'
-    req.body = entry.to_s
-    auth.authorize(req)
-    Net::HTTP.start('b.hatena.ne.jp', 80) do |http|
-      res = http.request(req)
-    end
-  end
-
-  add_completion do |input|
-    case input
-    when /^(hatebu)\s+(\d*)$/
-      find_status_id_candidates $2, "#{$1} %s"
-    else
-      %w(hatebu).grep(/^#{Regexp.quote input}/)
-    end
-  end
+  register_command(
+    :name => :hatebu, :aliases => [],
+    :exec_proc => proc {|arg|
+      if arg =~ /^(\d+)(.*)$/
+        id = $1.strip
+        comment = $2.strip
+        statuses = public_storage[:log].select { |s| s.id == id }
+        unless statuses.empty?
+          status = statuses.first
+        else
+          status = t.show(id).first
+        end
+        auth = auth = Atompub::Auth::Wsse.new({
+            :username => configatron.plugins.hatebu.username,
+            :password => configatron.plugins.hatebu.password,
+        })
+        link = Atom::Link.new({
+           :href => "http://twitter.com/#{status.user_screen_name}/status/#{status.id}",
+           :rel => 'related',
+           :type => 'text/html',
+        })
+        entry = Atom::Entry.new({
+            :link => link,
+            :title => 'dummy',
+            :summary => comment,
+        })
+        req = Net::HTTP::Post.new 'http://b.hatena.ne.jp/atom/post'
+        req['User-Agent'] = 'Mozilla/5.0'
+        req['Content-Type'] = 'application/atom+xml'
+        req['Slug'] = 'termtter'
+        req.body = entry.to_s
+        auth.authorize(req)
+        Net::HTTP.start('b.hatena.ne.jp', 80) do |http|
+          res = http.request(req)
+        end
+      end
+    },
+    :completion_proc => proc {|cmd, args|
+      if args =~ /^(\d*)$/
+        find_status_id_candidates $1, "#{cmd} %s"
+      end
+    },
+	:help => ['hatebu ID', 'Hatena bookmark a status']
+  )
 end
 
 # hatebu.rb
