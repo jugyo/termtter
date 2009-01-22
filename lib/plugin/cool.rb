6fad6d811c33a4069ab8709a38acf5619f276d7f
jugyo <kkohno@gmail.com>
2009-01-22 22:36:39 +0900

    fix bug that completion does not work

--- lib/plugin/cool.rb
+++ lib/plugin/cool.rb
@@ -1,8 +1,8 @@
-Termtter::Client.register_macro(:cool, "update @%s cool.",
-  :help => ['cool {SCREENNAME}', 'update "@{SCREENNAME} cool."'],
-  :completion_proc => proc {|cmd, args|
-    if args =~ /^([^\s]+)$/
-      find_user_candidates $1, "#{cmd} %s"
-    end
-  }
-)
+module Termtter::Client
+  register_macro(:cool, "update @%s cool.",
+    :help => ['cool {SCREENNAME}', 'update "@{SCREENNAME} cool."'],
+    :completion_proc => proc {|cmd, args|
+      find_user_candidates args, "#{cmd} %s"
+    }
+  )
+end
