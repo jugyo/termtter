e0f13642b960a439fb5588518d9df51127c791e7
jugyo <kkohno@gmail.com>
2009-01-22 22:59:21 +0900

    add_macro => register_command

--- spec/plugin/shell_spec.rb
+++ spec/plugin/shell_spec.rb
@@ -2,8 +2,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Termtter::Client, 'when the plugin shell is loaded' do
   it 'should add command shell' do
-    Termtter::Client.should_receive(:add_help)
-    Termtter::Client.should_receive(:add_macro).with(/^(?:shell|sh)\s*$/, "eval system ENV['SHELL'] || ENV['COMSPEC']")
+    Termtter::Client.should_receive(:register_command)
     plugin 'shell'
   end
 end
