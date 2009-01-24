34638179ef179f9c4e4485cd10b788ad85bd52f1
takano32 <takano32@gmail.com>
2009-01-24 14:39:30 +0900

    Revert "implement april fool plugin"
    This reverts commit 6f6ac1fe6440e4a01a262b42338d1edaddac7312.

--- lib/plugin/april_fool.rb
+++
@@ -1,8 +0,0 @@
-def april_fool?;true;end
-def april_fool;april_fool? ? "今日はエイプリルフールではありません。" : "
-今日はエイプリルフールです。";end
-module Termtter::Client
-add_command /^af\?/ do|m,t|t.update_status april_fool
-puts"=> #{april_fool}"end
-add_command /^af\?you\s(\w+)/ do|m,t|puts"=> #{t.update_status("@#{m[1]} #{april_fool}")}"end end
-# TODO: use add_macro
