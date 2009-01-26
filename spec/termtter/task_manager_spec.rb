d9c317716492c36d0ed74b481fbe3209eec83937
jugyo <kkohno@gmail.com>
2009-01-26 19:18:51 +0900

    add spec 'should run tasks'

--- spec/termtter/task_manager_spec.rb
+++ spec/termtter/task_manager_spec.rb
@@ -26,7 +26,24 @@ module Termtter
     end
 
     it 'should run tasks' do
-      
+      time_now = Time.now
+      Time.stub!(:now).and_return(time_now)
+
+      task1_called = false
+      task2_called = false
+      @task_manager.add_task() {task1_called = true}
+      @task_manager.add_task(:after => 10) {task2_called = true}
+
+      task1_called.should == false
+      task2_called.should == false
+      @task_manager.run
+      sleep 1
+      task1_called.should == true
+      task2_called.should == false
+
+      Time.stub!(:now).and_return(time_now + 10)
+      sleep 1
+      task2_called.should == true
     end
   end
 end
