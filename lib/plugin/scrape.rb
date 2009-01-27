4e78938939622ac89f24f0ca70e706029e9f2769
hitode909 <hitode909@gmail.com>
2009-01-27 20:39:18 +0900

    change to new type command registration.

--- lib/plugin/scrape.rb
+++ lib/plugin/scrape.rb
@@ -1,43 +1,34 @@
 module Termtter::Client
-  def self.scrape_group(group, t)
+  def self.scrape_group(group)
     members = configatron.plugins.group.groups[group] || []
-    members.map {|member|
-      t.get_user_timeline(member)
-    }.flatten
-  end
-
-  def self.scrape_groups(t)
-    groups = configatron.plugins.group.groups
-    groups.map {|group|
-      scrape_group(group, t)
-    }.flatten
-  end
-
-  add_help 'scrape_group GROUPNAME', 'Get the group timeline'
-  add_help 'scrape_groups', 'Get all groups timeline'
-
-  add_command /^(?:scrape_group)\s+(.+)/ do |m, t|
-    group_name = m[1].to_sym
-    statuses = scrape_group(group_name, t)
-    call_hooks(statuses, :pre_filter, t)
-    puts "done"
-  end
-
-  add_command /^(?:scrape_groups)\s*$/ do |m, t|
-    statuses = scrape_groups(t)
-    call_hooks(statuses, :pre_filter, t)
-    puts "done"
-  end
-
-  add_completion do |input|
-    case input
-    when /^(scrape_group)?\s+(.+)/
-      find_group_candidates($2, "#{$1} %s") if defined? find_group_candidates
-    when /^(scrape_group)\s+$/
-      configatron.plugins.group.groups.keys
-    else
-      %w(scrape_group scrape_groups).grep(/^#{Regexp.quote input}/)
+    statuses = []
+    members.each_with_index do |member, index|
+      puts "member #{index+1}/#{members.size} #{member}"
+      statuses += Termtter::API.twitter.get_user_timeline(member)
     end
+    statuses
   end
 
+  register_command(
+                   :name => :scrape_group,
+                   :exec_proc => proc{ |args|
+                     groups = args.split(' ').map{|g| g.to_sym}
+                     if groups.include? :all
+                       groups = configatron.plugins.group.groups.keys
+                       puts "get all groups..."
+                     end
+                     statuses = []
+                     groups.each_with_index do |group, index|
+                       puts "group #{index+1}/#{groups.size} #{group}"
+                       statuses += scrape_group(group)
+                     end
+                     call_hooks(statuses, :pre_filter)
+                   },
+                   :completion_proc => proc {|cmd, args|
+                     arg = args.split(' ').last
+                     prefix = args.split(' ')[0..-2].join(' ')
+                     find_group_candidates arg, "#{cmd} #{prefix} %s"
+                   },
+                   :help => ['scrape_group GROUPNAME(S)', 'Get the group timeline']
+                   )
 end
