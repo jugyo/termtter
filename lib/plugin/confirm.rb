cf2a2b3a14cf769887abe4ce68fc6e5ba0c6bf04
jugyo <kkohno@gmail.com>
2009-01-27 23:12:18 +0900

    change to use pre_exec_ hook

--- lib/plugin/confirm.rb
+++ lib/plugin/confirm.rb
@@ -1,24 +1,7 @@
-module Termtter::Client
-
-  add_command /^confirm\s+(.*)/ do |m, t|
-    text = m[1]
-    unless text.empty?
-      pause
-      print "update? #{text} [Y/n] "
-      buf = Readline.readline("", false)
-      t.update_status(text) if /^y?$/i =~ buf
-      resume
-    end
-  end
-
-  add_completion do |input|
-    case input
-    when /^(confirm)\s+(.*)@([^\s]*)$/
-      find_user_candidates $3, "#{$1} #{$2}@%s"
-    else
-      ['confirm'].grep(/^#{Regexp.quote input}/)
-    end
-  end
-
-end
-
+Termtter::Client.register_hook(
+  :name => :pre_exec_hook_sample,
+  :points => [:pre_exec_update],
+  :exec_proc => proc {|cmd, arg|
+    false if /^y?$/i !~ Readline.readline("update? #{arg} [Y/n] ", false)
+  }
+)
