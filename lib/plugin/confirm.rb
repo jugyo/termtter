13ec815fb6c085d55c7c140d71b13d4ec10c4a3e
jugyo <kkohno@gmail.com>
2009-01-29 00:22:50 +0900

    change to return false if arg is empty

--- lib/plugin/confirm.rb
+++ lib/plugin/confirm.rb
@@ -2,6 +2,6 @@ Termtter::Client.register_hook(
   :name => :confirm,
   :points => [:pre_exec_update],
   :exec_proc => proc {|cmd, arg|
-    false if /^y?$/i !~ Readline.readline("update? #{arg} [Y/n] ", false)
+    false if arg.empty? || /^y?$/i !~ Readline.readline("update? #{arg} [Y/n] ", false)
   }
 )
