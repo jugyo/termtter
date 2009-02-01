0e9817dfeeba2d7b326bec8b3179e1944a62f3d3
hitode909 <hitode909@gmail.com>
2009-02-01 22:57:21 +0900

    change to new type command registration.

--- lib/plugin/yonda.rb
+++ lib/plugin/yonda.rb
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
 module Termtter::Client
-  public_storage[:unread_count] = 0
-
-  add_help 'yonda,y', 'Mark as read'
-
-  add_command %r'^y(?:onda)?\s*$' do |m, t|
-    public_storage[:unread_count] = 0
-    print "\033[2J\033[H" # FIXME
-    call_hooks [], :plugin_yonda_yonda, t
-  end
+  register_command(
+   :name => :yonda, :aliases => [:y],
+   :exec_proc => proc{|arg|
+     public_storage[:unread_count] = 0
+     print "\033[2J\033[H" # FIXME
+     call_hooks [], :plugin_yonda_yonda
+   },
+   :help => ['yonda,y', 'Mark as read']
+   )
 
   add_hook do |statuses, event|
     case event
