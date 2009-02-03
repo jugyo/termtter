dcab3df33b170e73d4a9ee7ef5fd7e07a04c3474
mattn <mattn.jp@gmail.com>
2009-02-03 14:05:00 +0900

    change to use register_command.

--- lib/plugin/devel.rb
+++ lib/plugin/devel.rb
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 module Termtter::Client
-  # TODO: use register_command
-  add_command /^eval\s+(.*)$/ do |m, t|
-    begin
-      result = eval(m[1]) unless m[1].empty?
-      puts "=> #{result.inspect}"
-    rescue SyntaxError => e
-      puts e
-    end
-  end
+  register_command(
+   :name => :eval,
+   :aliases => [],
+   :exec_proc => lambda {|arg|
+     result = eval(arg) unless arg.empty?
+     puts "=> #{result.inspect}"
+   },
+   :help => ['eval EXPR', 'evaluate expression']
+  )
 end
