80836950d625232eda5e274ed9926d885c9f1c80
Tomohiro Nishimura <tomohiro68@gmail.com>
2009-03-14 14:31:52 +0900

    refactored

--- lib/termtter/command.rb
+++ lib/termtter/command.rb
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 module Termtter
+
   class Command
+
     attr_accessor :name, :aliases, :exec_proc, :completion_proc, :help
 
     # args
@@ -12,11 +14,21 @@ module Termtter
     #   help:            help text for the command (Optional)
     def initialize(args)
       raise ArgumentError, ":name is not given." unless args.has_key?(:name)
-      @name = args[:name].to_sym
-      @aliases = args[:aliases] ? args[:aliases].map {|i| i.to_sym } : []
-      @exec_proc = args[:exec_proc] || lambda {|arg|}
-      @completion_proc = args[:completion_proc] || lambda {|command, arg| [] }
-      @help = args[:help]
+      config = {
+        :aliases        => [],
+        :exec_proc      => lambda {|arg| },
+        :comletion_proc => lambda {|command, arg| [] }
+      }.merge(args)
+
+      set config
+    end
+
+    def set(config)
+      @name            = config[:name].to_sym
+      @aliases         = config[:aliases]
+      @exec_proc       = config[:exec_proc]
+      @completion_proc = config[:completion_proc]
+      @help            = config[:help]
     end
 
     def complement(input)
@@ -24,7 +36,7 @@ module Termtter
       if command_info
         [completion_proc.call(command_info[0], command_info[1] || '')].flatten.compact
       else
-        [name.to_s].grep(/^#{Regexp.quote(input)}/)
+        [name.to_s, aliases.to_s].grep(/^#{Regexp.quote(input)}/)
       end
     end
 
@@ -43,15 +55,6 @@ module Termtter
       end
     end
 
-    # return array like [command, arg]
-    def match?(input)
-      if pattern =~ input
-        [$2 || $3, $4]  # $2 or $3 => command, $4 => argument
-      else
-        nil
-      end
-    end
-
     def execute(arg)
       arg = case arg
         when nil
@@ -64,8 +67,17 @@ module Termtter
       exec_proc.call(arg)
     end
 
+    # return array like [command, arg]
+    def match?(input)
+      if pattern =~ input
+        [$2 || $3, $4]  # $2 or $3 => command, $4 => argument
+      else
+        nil
+      end
+    end
+
     def pattern
-      commands_regex = commands.map {|i| Regexp.quote(i.to_s) }.join('|')
+      commands_regex = commands.map {|name| Regexp.quote(name.to_s) }.join('|')
       /^((#{commands_regex})|(#{commands_regex})\s+(.*?))\s*$/
     end
 
