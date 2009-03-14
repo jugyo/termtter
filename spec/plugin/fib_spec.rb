f89d5ca6692c73c882bd7d156216371c7061cd23
Koichiro Ohba <koichiro@meadowy.org>
2009-03-14 22:18:17 +0900

    fix spec

--- spec/plugin/fib_spec.rb
+++ spec/plugin/fib_spec.rb
@@ -4,7 +4,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Termtter::Client, 'when the plugin fib is loaded' do
   it 'should add command fib' do
-    Termtter::Client.should_receive(:register_command)
+    Termtter::Client.should_receive(:register_command).twice
     plugin 'fib'
   end
 
