6932a5b41d99345eb5a48d36f2af1178d079321f
Tomohiro Nishimura <tomohiro68@gmail.com>
2009-03-14 17:19:19 +0900

    write spec fo config library

--- spec/termtter/config_spec.rb
+++ spec/termtter/config_spec.rb
@@ -1,19 +1,55 @@
 # -*- coding: utf-8 -*-
 
 require File.dirname(__FILE__) + '/../spec_helper'
-require File.dirname(__FILE__) + '/../../lib/termtter/config.rb'
 
 module Termtter::Config
 
   describe Storage do
 
     before do
-      @strage = Storage.new('config')
+      @storage = Storage.new('config')
     end
 
-    it 'should be add value to new strage' do
-      @strage.new_strage = :value
-      @strage.new_strage.should == :value
+    it 'should be able to store value to new storage' do
+      @storage.new_storage = :value
+      @storage.new_storage.should == :value
+    end
+
+    it 'should be able to make sub.key and store value' do
+      @storage.sub.key = :value
+      @storage.sub.key.should == :value
+    end
+
+    it 'should be able to change value in storage' do
+      @storage.storage = :value1
+      @storage.storage = :value2
+      @storage.storage.should == :value2
+    end
+
+    it 'should be able to store any data' do
+      [
+        ['string',  'value'   ],
+        ['symbol',  :value    ],
+        ['arrry',   [:a, :b]  ],
+        ['hash',    {:a => :b}],
+        ['integer', 1         ],
+        ['float',   1.5       ],
+        ['regexp',  /regexp/  ],
+      ].each do |type, value|
+        @storage.__send__("#{type}=", value)
+        @storage.__send__(type).should == value
+      end
+    end
+
+    it 'should raise error when add sub-storage to existed key' do
+      @storage.sub = 'original value'
+      lambda {
+        @storage.sub.key = 'invalid substitution'
+      }.should raise_error(
+        NoMethodError,
+        %r[undefined method `key=' for "original value":String]
+      )
     end
   end
 end
+
