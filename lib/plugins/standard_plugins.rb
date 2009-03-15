848abf8d2f3dc3b0648d008a9d3113c99b8845a4
jugyo <jugyo.org@gmail.com>
2009-03-15 16:30:36 +0900

    Move follow and leave

--- lib/plugins/standard_plugins.rb
+++ lib/plugins/standard_plugins.rb
@@ -133,6 +133,36 @@ module Termtter::Client
     :completion_proc => get_command(:show).completion_proc
   )
 
+  register_command(
+    :name => :follow, :aliases => [],
+    :exec_proc => lambda {|args|
+      args.split(' ').each do |arg|
+        if arg =~ /^(\w+)/
+          res = Termtter::API::twitter.follow($1.strip)
+        end
+      end
+    },
+    :completion_proc => lambda {|cmd, args|
+      find_user_candidates args, "#{cmd} %s"
+    },
+	:help => ['follow USER', 'Follow user']
+  )
+
+  register_command(
+    :name => :leave, :aliases => [],
+    :exec_proc => lambda {|args|
+      args.split(' ').each do |arg|
+        if arg =~ /^(\w+)/
+          res = Termtter::API::twitter.leave($1.strip)
+        end
+      end
+    },
+    :completion_proc => lambda {|cmd, args|
+      find_user_candidates args, "#{cmd} %s"
+    },
+	:help => ['leave USER', 'Leave user']
+  )
+
   # TODO: Change colors when remaining_hits is low.
   # TODO: Simmulate remaining_hits.
   register_command(
