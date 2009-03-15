3c40fb89ee577119420e0d23731341b235329c0e
ujihisa <ujihisa@gmail.com>
2009-03-15 14:16:06 +0900

    enable to use english_filter

--- lib/plugins/english_filter.rb
+++ lib/plugins/english_filter.rb
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
+# vim: set fenc=utf-8
+
+module Termtter::English
+  # english? :: String -> Boolean
+  def self.english?(message)
+    /[一-龠]+|[ぁ-ん]+|[ァ-ヴー]+|[ａ-ｚＡ-Ｚ０-９]+/ !~ message
+  end
+end
 
 Termtter::Client.add_filter do |statuses|
-  statuses.select{|s| s.english? }
+  statuses.select {|i| Termtter::English.english?(i.text) }
 end
 
 # english_filter.rb
-#   select English post only
+#   select English posts only
