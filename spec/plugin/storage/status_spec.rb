e26296fb23fea800b0cbef17ebdbcf859a7b0fc4
ujihisa <ujihisa@gmail.com>
2009-03-15 09:19:14 +0900

    plugin to plugins

--- spec/plugin/storage/status_spec.rb
+++ spec/plugin/storage/status_spec.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 require File.dirname(__FILE__) + '/../../spec_helper'
-require File.dirname(__FILE__) + '/../../../lib/plugin/storage/status'
+require File.dirname(__FILE__) + '/../../../lib/plugins/storage/status'
 
 module Termtter::Storage
   describe Status do
