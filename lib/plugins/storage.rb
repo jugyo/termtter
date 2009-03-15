09bbec18953025c6cd3565cd6b64c243c70ebd40
ujihisa <ujihisa@gmail.com>
2009-03-15 15:19:33 +0900

    refactoring

--- lib/plugins/storage.rb
+++ lib/plugins/storage.rb
@@ -13,36 +13,33 @@ module Termtter::Client
     when :pre_filter
       statuses.each do |s|
         Termtter::Storage::Status.insert(
-                                         {
-                                           :post_id => s.id,
-                                           :created_at => Time.parse(s.created_at).to_i, 
-                                           :in_reply_to_status_id => s.in_reply_to_status_id,
-                                           :in_reply_to_user_id => s.in_reply_to_user_id,
-                                           :post_text => s.text,
-                                           :user_id => s.user.id,
-                                           :screen_name => s.user.screen_name
-                                         }
-                                         )
+          {
+          :post_id => s.id,
+          :created_at => Time.parse(s.created_at).to_i, 
+          :in_reply_to_status_id => s.in_reply_to_status_id,
+          :in_reply_to_user_id => s.in_reply_to_user_id,
+          :post_text => s.text,
+          :user_id => s.user.id,
+          :screen_name => s.user.screen_name
+        })
       end
     end
   end
 
   register_command(
-                   :name => :search_storage, :aliases => [:ss],
-                   :exec_proc => lambda{|arg|
-                     unless arg.strip.empty?
-                       key = arg.strip
-                       
-                       statuses = public_storage[:storage].search({ :post_text => key})
-                       
-                       statuses.each do |s|
-                         p s
-                       end
-                       call_hooks(statuses, :search)
-                       
-                     end
-                   },
-                   
-                   :help => [ 'search_storage WORD', 'Search storage for WORD' ]
-                   )
+    :name => :search_storage, :aliases => [:ss],
+    :exec_proc => lambda {|arg|
+        unless arg.strip.empty?
+          key = arg.strip
+
+          statuses = public_storage[:storage].search({ :post_text => key})
+
+          statuses.each do |s|
+            p s
+          end
+          call_hooks(statuses, :search)
+
+        end
+      },
+    :help => [ 'search_storage WORD', 'Search storage for WORD' ])
 end
