a5bc26b66ecacaad2dd50f4063581af80b9b4c32
ujihisa <ujihisa@gmail.com>
2009-03-15 11:07:23 +0900

    suspend warning of dupple assignments of constants caused by dupple requirement

--- spec/plugin/plugin_spec.rb
+++ spec/plugin/plugin_spec.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-require File.dirname(__FILE__) + '/../../lib/termtter'
+require File.dirname(__FILE__) + '/../spec_helper'
 
 module Termtter
   describe Client, 'when the plugin plugin is loaded' do
