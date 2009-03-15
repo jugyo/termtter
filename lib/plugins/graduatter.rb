679f89afd18cc5e310623b8981df252dd5c71b6b
jugyo <jugyo.org@gmail.com>
2009-03-15 14:58:59 +0900

    Change to use rubytter

--- lib/plugins/graduatter.rb
+++ lib/plugins/graduatter.rb
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
-twitter = Termtter::Twitter.new(config.user_name, config.password)
 Thread.start do
   100.times do |i|
-    twitter.update_status(
+    Twitter::API.twitter.update(
       "I decided not to use twitter so as not to leave university before I complete the dissertation#{i.odd? ? '!' : '.'}")
   end
 end
