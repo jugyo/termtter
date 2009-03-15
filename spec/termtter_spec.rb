d96d9e098489249fa448558e415abfde023aad92
jugyo <jugyo.org@gmail.com>
2009-03-15 09:32:48 +0900

    Delete a unused spec.

--- spec/termtter_spec.rb
+++ spec/termtter_spec.rb
@@ -3,15 +3,4 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 module Termtter
-  describe Twitter, 'when get_timeline called' do
-    before do
-      connection = mock('connection', :null_object => true)
-      @twitter = Termtter::Twitter.new('test', 'test', connection)
-
-      Termtter::Client.add_hook do |statuses, event|
-        @statuses = statuses
-        @event = event
-      end
-    end
-  end
 end
