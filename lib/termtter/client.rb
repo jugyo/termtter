4e8280cf294e6d531f29aca05ef4cc8b53529e62
jugyo <jugyo.org@gmail.com>
2009-03-17 19:39:48 +0900

    Changed to init config file

--- lib/termtter/client.rb
+++ lib/termtter/client.rb
@@ -218,17 +218,9 @@ module Termtter
           username = ui.ask('your twitter username: ')
           password = ui.ask('your twitter password: ') { |q| q.echo = false }
 
-          Dir.mkdir(Termtter::CONF_DIR)
+          Dir.mkdir(Termtter::CONF_DIR) unless File.exists?(Termtter::CONF_DIR)
           File.open(Termtter::CONF_FILE, 'w') {|io|
             io.puts '# -*- coding: utf-8 -*-'
-
-            plugins = Dir.glob(File.dirname(__FILE__) + "/../lib/plugins/*.rb").map  {|f|
-              f.match(%r|lib/plugin/(.*?).rb$|)[1]
-            }
-            plugins -= %w[stdout standard_plugins]
-            plugins.each do |p|
-              io.puts "#plugin '#{p}'"
-            end
 
             io.puts
             io.puts "config.user_name = '#{username}'"
@@ -238,6 +230,14 @@ module Termtter
             io.puts "#config.proxy.port = '8080'"
             io.puts "#config.proxy.user_name = 'proxy user'"
             io.puts "#config.proxy.password = 'proxy password'"
+            io.puts
+            plugins = Dir.glob(File.expand_path(File.dirname(__FILE__) + "/../plugins/*.rb")).map  {|f|
+              f.match(%r|lib/plugins/(.*?).rb$|)[1]
+            }
+            plugins -= %w[stdout standard_plugins]
+            plugins.each do |p|
+              io.puts "#plugin '#{p}'"
+            end
             io.puts
             io.puts "# vim: set filetype=ruby"
           }
