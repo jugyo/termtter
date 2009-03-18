4962bed68ec3c063c5568c5a27682f42d107e9f1
jugyo <jugyo.org@gmail.com>
2009-03-19 01:31:47 +0900

    Add output method.

--- lib/plugins/stdout.rb
+++ lib/plugins/stdout.rb
@@ -11,68 +11,36 @@ config.plugins.stdout.set_default(
   '<90><%=time%></90> <<%=status_color%>><%=status%></<%=status_color%>> <90><%=id%></90>')
 config.plugins.stdout.set_default(:search_highlihgt_format, '<on_magenta><white>\1</white></on_magenta>')
 
-$highline = HighLine.new
+module Termtter
+  class StdOut < Hook
+    def initialize
+      super(:name => :stdout, :points => [:output])
+    end
 
-def color(str, value)
-  return str if value == :none
-  case value
-  when String, Symbol
-    $highline.color(str, value)
-  else
-    "\e[#{value}m#{str}\e[0m"
-  end
-end
+    def execute(statuses, event)
+      print_statuses(statuses)
+    end
 
-module Termtter::Client
+    def print_statuses(statuses, sort = true, time_format = '%H:%M:%S')
+      (sort ? statuses.sort_by{ |s| s[:id]} : statuses).each do |s|
+        text = s[:text]
+        status_color = config.plugins.stdout.colors[s[:user_id].hash % config.plugins.stdout.colors.size]
+        status = "#{s[:screen_name]}: #{text}"
+        if s[:reply_to]
+          status += " (repl. to #{s[:reply_to]})"
+        end
 
-  def self.print_statuses(statuses, sort = true, time_format = '%H:%M:%S')
-    (sort ? statuses.sort_by{ |s| s.id} : statuses).each do |s|
-      text = s.text
-      status_color = config.plugins.stdout.colors[s.user.screen_name.hash % config.plugins.stdout.colors.size]
-      status = "#{s.user.screen_name}: #{text}"
-      if s.in_reply_to_status_id
-        status += " (repl. to #{s.in_reply_to_status_id})"
+        time = "(#{Time.parse(s[:created_at]).strftime(time_format)})"
+        id = s[:id]
+        erbed_text = ERB.new(config.plugins.stdout.timeline_format).result(binding)
+        puts TermColor.parse(erbed_text)
       end
-
-      time = "(#{Time.parse(s.created_at).strftime(time_format)})"
-      id = s.id
-      erbed_text = ERB.new(config.plugins.stdout.timeline_format).result(binding)
-      puts TermColor.parse(erbed_text)
     end
   end
 
-  def self.print_statuses_with_date(statuses, sort = true)
-    print_statuses(statuses, sort, '%m-%d %H:%M')
-  end
-
-  def self.print_search_results(result, time_format = '%H:%M:%S')
-    result.results.sort_by{|r| r.created_at}.each do |r|
-      text = r.text.
-                gsub(/(\n|\r)/, '').
-                gsub(/(#{Regexp.escape(result.query)})/i, config.plugins.stdout.search_highlihgt_format)
-      status_color = config.plugins.stdout.colors[r.from_user_id.to_i.hash % config.plugins.stdout.colors.size]
-      status = "#{r.from_user}: #{text}"
-      time = "(#{Time.parse(r.created_at).strftime(time_format)})"
-      id = r.id
-      erbed_text = ERB.new(config.plugins.stdout.timeline_format).result(binding)
-      puts TermColor.parse(erbed_text)
-    end
-  end
-
-  add_hook do |result, event|
-    case event
-    when :update_friends_timeline, :list_friends_timeline
-      print_statuses(result) unless result.empty?
-    when :list_user_timeline, :replies
-      print_statuses_with_date(result) unless result.empty?
-    when :show
-      print_statuses_with_date([result])
-    when :search
-      print_search_results(result)
-    end
-  end
-
+  Client.register_hook(StdOut.new)
 end
+
 # stdout.rb
 #   output statuses to stdout
 # example config
