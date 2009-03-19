240c77fd3c4fc86132983944001e10bac753a44c
jugyo <jugyo.org@gmail.com>
2009-03-20 00:58:30 +0900

    Changed to use register_hook.

--- lib/plugins/storage.rb
+++ lib/plugins/storage.rb
@@ -8,21 +8,23 @@ require File.dirname(__FILE__) + '/storage/status'
 module Termtter::Client
   public_storage[:log] = []
 
-  add_hook do |statuses, event|
-    case event
-    when :pre_filter
+  register_hook(
+    :name => :storage,
+    :points => [:pre_filter],
+    :exec_proc => lambda {|statuses, event|
       statuses.each do |s|
         Termtter::Storage::Status.insert(
-          :post_id => s.id,
-          :created_at => Time.parse(s.created_at).to_i,
-          :in_reply_to_status_id => s.in_reply_to_status_id,
-          :in_reply_to_user_id => s.in_reply_to_user_id,
-          :post_text => s.text,
-          :user_id => s.user.id,
-          :screen_name => s.user.screen_name)
+          :post_id => s[:id],
+          :created_at => Time.parse(s[:created_at]).to_i,
+          :in_reply_to_status_id => s[:in_reply_to_status_id],
+          :in_reply_to_user_id => s[:in_reply_to_user_id],
+          :post_text => s[:post_text],
+          :user_id => s[:user_id],
+          :screen_name => s[:screen_name]
+        )
       end
-    end
-  end
+    }
+  )
 
   register_command(
     :name => :search_storage,
