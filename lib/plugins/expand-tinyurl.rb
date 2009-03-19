7103735a94b2bfc3c22d5374f30412a5eaeff975
Koichiro Ohba <koichiro@meadowy.org>
2009-03-19 23:29:39 +0900

    Added support for http://is.gd expand url.

--- lib/plugins/expand-tinyurl.rb
+++ lib/plugins/expand-tinyurl.rb
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 
+URL_SHORTTERS = [
+  { :host => "tinyurl.com", :pattern => %r'(http://tinyurl\.com(/[\w/]+))' },
+  { :host => "is.gd", :pattern => %r'(http://is\.gd(/[\w/]+))' },
+#  { :host => "bit.ly", :pattern => %r'(http://bit\.ly(/[\w/]+))' }
+]
+
 module Termtter::Client
   add_filter do |statuses, event|
     statuses.each do |s|
-      s.text.gsub!(%r'(http://tinyurl\.com(/[\w/]+))') do |m|
-        expand_tinyurl($2) || $1
+      URL_SHORTTERS.each do |site|
+        s[:post_text].gsub!(site[:pattern]) do |m|
+          expand_url(site[:host], $2) || $1
+        end
       end
     end
     statuses
   end
 end
 
-def expand_tinyurl(path)
+def expand_url(host, path)
   http_class = Net::HTTP
   unless config.proxy.host.nil? or config.proxy.host.empty?
     http_class = Net::HTTP::Proxy(config.proxy.host,
@@ -20,5 +28,6 @@ def expand_tinyurl(path)
                                   config.proxy.password)
   end
   res = http_class.new('tinyurl.com').head(path)
+  return nil unless res.code == "301" or res.code == "302"
   res['Location']
 end
