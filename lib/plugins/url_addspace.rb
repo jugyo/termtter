4f24f3ba5f9ad4cfeae4ad4543e74f36279bb573
hitode909 <hitode909@gmail.com>
2009-03-21 04:16:10 +0900

    url_addspaceが動かなかったのを修正

--- lib/plugins/url_addspace.rb
+++ lib/plugins/url_addspace.rb
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 module Termtter::Client
-  add_filter do |statuses|
+  add_filter do |statuses, event|
     statuses.each do |s|
-      s.text.gsub!(/(\S)(https?:\/\/)/, '\1 \2')
+      s[:post_text].gsub!(/(\S)(https?:\/\/)/, '\1 \2')
     end
     statuses
   end
