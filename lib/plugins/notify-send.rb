0aa64a70bfedad29cf5e80a4f4d760212c97d136
jugyo <kkohno@gmail.com>
2009-03-21 14:50:23 +0900

    Changed to use register_hook.

--- lib/plugins/notify-send.rb
+++ lib/plugins/notify-send.rb
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 
-Termtter::Client.add_hook do |statuses, event|
-  if !statuses.empty? && event == :update_friends_timeline
-    max = 10
+Termtter::Client.register_hook(
+  :name => :notify_send,
+  :points => [:post_filter],
+  :exec_proc => lambda {|statuses, event|
+    if event == :update_friends_timeline
+      max = 10
 
-    text = statuses.take(max).map {|s|
-      status_text = CGI.escapeHTML(s.text)
-      status_text.gsub!(%r{https?://[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+},'<a href="\0">\0</a>')
-      "<b>#{s.user.screen_name}:</b> <span font=\"9.0\">#{status_text}</span>"
-    }.join("\n")
+      text = statuses.take(max).map {|s|
+        status_text = CGI.escapeHTML(s[:post_text])
+        status_text.gsub!(%r{https?://[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+},'<a href="\0">\0</a>')
+        "<b>#{s[:screen_name]}:</b> <span font=\"9.0\">#{status_text}</span>"
+      }.join("\n")
 
-    text << %Q|\n<a href="http://twitter.com/">more...</a>| if statuses.size > max
+      text << %Q|\n<a href="http://twitter.com/">more...</a>| if statuses.size > max
 
-    system 'notify-send', 'Termtter', text, '-t', '60000'
-  end
-end
+      system 'notify-send', 'Termtter', text, '-t', '60000'
+    end
+  }
+)
+
