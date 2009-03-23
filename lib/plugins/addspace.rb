d6280734441245d344a6ef6aa67ae96c515756cc
tomisima <tomisima@gmail.com>
2009-03-23 17:00:04 +0900

    add '…' to after and modify regexp

--- lib/plugins/addspace.rb
+++ lib/plugins/addspace.rb
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
 config.plugins.addspace.set_default( :before, [ %r{https?://} ] )
-config.plugins.addspace.set_default( :after, %w{ ★ ☆ △ ▽})
+config.plugins.addspace.set_default( :after, %w{ ★ ☆ △ ▽ … })
 
 module Termtter::Client
   add_filter do |statuses, event|
     statuses.each do |s|
       config.plugins.addspace.before.each do |c|
-        s.text.gsub!(/(\S)(#{c})/, '\1 \2' )
+        s.text.gsub!(/(?=\S)(#{c})/, ' \1' )
       end
     end
     statuses.each do |s|
       config.plugins.addspace.after.each do |c|
-        s.text.gsub!(/(#{c})(\S)/, '\1 \2' )
+        s.text.gsub!(/(#{c})(?=\S)/, '\1 ' )
       end
       statuses
     end
