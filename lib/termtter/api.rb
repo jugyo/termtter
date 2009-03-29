f73b31395fa635ab6ce434cf52d314577241af32
jugyo <jugyo.org@gmail.com>
2009-03-29 23:54:38 +0900

    Implemented switch_user and restore_user.

--- lib/termtter/api.rb
+++ lib/termtter/api.rb
@@ -16,49 +16,40 @@ module Termtter
       attr_reader :connection, :twitter, :twitter_old
       def setup
         @connection = Connection.new
-        @twitter = Rubytter.new(
-                    config.user_name,
-                    config.password,
-                    {
-                      :app_name => Termtter::APP_NAME,
-                      :host => config.host,
-                      :header => {
-                        'User-Agent' => 'Termtter http://github.com/jugyo/termtter',
-                        'X-Twitter-Client' => 'Termtter',
-                        'X-Twitter-Client-URL' => 'http://github.com/jugyo/termtter',
-                        'X-Twitter-Client-Version' => Termtter::VERSION
-                      },
-                      :enable_ssl => config.enable_ssl,
-                      :proxy_host => config.proxy.host,
-                      :proxy_port => config.proxy.port,
-                      :proxy_user_name => config.proxy.user_name,
-                      :proxy_password => config.proxy.password
-                    }
-                  )
+        @twitter = create_twitter(config.user_name, config.password)
       end
+
+      def restore_user
+        @twitter = create_twitter(config.user_name, config.password)
+      end
+
       def switch_user(username = nil)
         highline = create_highline
-        username = highline.ask('your twitter username: ') unless username
+        username = highline.ask('your twitter username: ') if username.nil? || username.empty?
         password = highline.ask('your twitter password: ') { |q| q.echo = false }
-        @twitter = Rubytter.new(
-                    username,
-                    password,
-                    {
-                      :app_name => Termtter::APP_NAME,
-                      :host => config.host,
-                      :header => {
-                        'User-Agent' => 'Termtter http://github.com/jugyo/termtter',
-                        'X-Twitter-Client' => 'Termtter',
-                        'X-Twitter-Client-URL' => 'http://github.com/jugyo/termtter',
-                        'X-Twitter-Client-Version' => Termtter::VERSION
-                      },
-                      :enable_ssl => config.enable_ssl,
-                      :proxy_host => config.proxy.host,
-                      :proxy_port => config.proxy.port,
-                      :proxy_user_name => config.proxy.user_name,
-                      :proxy_password => config.proxy.password
-                    }
-                  )
+        @twitter = create_twitter(username, password)
+      end
+
+      def create_twitter(user_name, password)
+        Rubytter.new(
+          user_name,
+          password,
+          {
+            :app_name => Termtter::APP_NAME,
+            :host => config.host,
+            :header => {
+              'User-Agent' => 'Termtter http://github.com/jugyo/termtter',
+              'X-Twitter-Client' => 'Termtter',
+              'X-Twitter-Client-URL' => 'http://github.com/jugyo/termtter',
+              'X-Twitter-Client-Version' => Termtter::VERSION
+            },
+            :enable_ssl => config.enable_ssl,
+            :proxy_host => config.proxy.host,
+            :proxy_port => config.proxy.port,
+            :proxy_user_name => config.proxy.user_name,
+            :proxy_password => config.proxy.password
+          }
+        )
       end
     end
   end
