cc1996f5b1fc639a4569db6b5ea2d8d22e843bd5
tomisima <tomisima@gmail.com>
2009-03-30 18:59:49 +0900

    uri-open風のリスト表示をするようにした

--- lib/plugins/u2.rb
+++ lib/plugins/u2.rb
@@ -2,16 +2,24 @@
 
 module Termtter::Client
   register_command(
-    :name => :update2, :aliases => [:u2],
+    :name => :reply,
     :exec_proc => lambda {|arg|
-      unless /^\s*$/ =~ arg
-        id, status = arg.split(' ', 2)
-        text = ERB.new(status).result(binding).gsub(/\n/, ' ')
-        result = if id =~ /^\d+$/
-                   Termtter::API.twitter.update(text, {'in_reply_to_status_id' => id})
-                 else
-                   Termtter::API.twitter.update(text)
-                 end
+      case arg
+      when /^\s*(?:list)?\s*$/
+        public_storage[:log].each_with_index do |s, i|
+          puts "#{i}: #{s.user.screen_name}: #{s.text}"
+        end
+      when /^\s*u(?:pdate)?\s+(\d+)\s+(.+)$/
+        id   = public_storage[:log][$1.to_i].id
+        user = public_storage[:log][$1.to_i].user.screen_name
+        text = ERB.new("@#{user} #{$2}").result(binding).gsub(/\n/, ' ')
+        result = Termtter::API.twitter.update(text, {'in_reply_to_status_id' => id})
+        puts "=> #{text}"
+        result
+      when /^\s*(\d+)\s+(.+)$/
+        id   = $1
+        text = ERB.new($2).result(binding).gsub(/\n/, ' ')
+        result = Termtter::API.twitter.update(text, {'in_reply_to_status_id' => id})
         puts "=> #{text}"
         result
       end
@@ -19,6 +27,33 @@ module Termtter::Client
     :completion_proc => lambda {|cmd, args|
       #todo
     },
-    :help => ["update2,u2 STATUS_ID TEXT", "Post a message to STATUS_ID"]
   )
 end
+
+=begin
+= Termtter reply command
+== Usage
+=== list
+* ステータスのリストを連番と一緒に出す。
+ command: reply [list]
+ > reply
+ 0: foo: foo's message
+ 1: bar: bar's message
+ ..
+
+=== reply
+* 上記listコマンドで出したステータスNOに対してメッセージを送る。
+* メッセージ送信の際、@usernameが自動的に付与される。
+ command: reply [u|update] status_no message
+ > reply u 0 message4foo
+
+=== reply
+* 対象のstatus_idを自分で入力してメッセージを送信する。
+ command: reply status_id message
+
+== Todo
+* 英語で説明
+* リファクタ
+* 補完
+* できたらファイル名をreply.rbにする。
+=end
