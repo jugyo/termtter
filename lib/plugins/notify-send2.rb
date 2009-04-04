2be35b162033965bd1d6238477658bbacd4d5386
jugyo <kkohno@gmail.com>
2009-04-04 21:59:44 +0900

    growl2 を真似した

--- lib/plugins/notify-send2.rb
+++ lib/plugins/notify-send2.rb
@@ -2,23 +2,36 @@
 
 require 'fileutils'
 
-# notify-send.rb からコピペ。共通化したいところ。
 config.plugins.notify_send.set_default(:icon_cache_dir, "#{Termtter::CONF_DIR}/tmp/user_profile_images")
+
+FileUtils.mkdir_p(config.plugins.notify_send.icon_cache_dir) unless File.exist?(config.plugins.notify_send.icon_cache_dir)
+Dir.glob("#{config.plugins.notify_send.icon_cache_dir}/*") {|f| File.delete(f) unless File.size?(f) }
+unless File.exist?("#{config.plugins.notify_send.icon_cache_dir}/default.png")
+  File.open("#{config.plugins.notify_send.icon_cache_dir}/default.png", "wb") do |f|
+    f << open("http://static.twitter.com/images/default_profile_normal.png").read
+  end
+end
+
 def get_icon_path(s)
-  FileUtils.mkdir_p(config.plugins.notify_send.icon_cache_dir) unless File.exist?(config.plugins.notify_send.icon_cache_dir)
-  cache_file = "%s/%s%s" % [  config.plugins.notify_send.icon_cache_dir, 
-                              s.user.screen_name, 
-                              File.extname(s.user.profile_image_url)  ]
-  if !File.exist?(cache_file) || (File.atime(cache_file) + 24*60*60) < Time.now
-    File.open(cache_file, "wb") do |f|
+  /https?:\/\/.+\/(\d+)\/.*?$/ =~ s.user.profile_image_url
+  cache_file = "%s/%s-%s%s" % [  config.plugins.notify_send.icon_cache_dir,
+                                 s.user.screen_name,
+                                 $+,
+                                 File.extname(s.user.profile_image_url)  ]
+  unless File.exist?(cache_file)
+    Thread.new(s,cache_file) do |s,cache_file|
+      Dir.glob("#{config.plugins.notify_send.icon_cache_dir}/#{s.user.screen_name}-*") {|f| File.delete(f) }
       begin
-        f << open(URI.escape(s.user.profile_image_url)).read
+        s.user.profile_image_url.sub!(/^https/,'http')
+        File.open(cache_file, 'wb') do |f|
+          f << open(URI.escape(s.user.profile_image_url)).read
+        end
       rescue OpenURI::HTTPError
-        return nil
+        cache_file = "#{config.plugins.notify_send.icon_cache_dir}/default.png"
       end
     end
   end
-  cache_file
+  return cache_file
 end
 
 Termtter::Client.register_hook(
