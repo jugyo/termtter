61599b080192c5f9ccdcb54618bc5ca07599ded6
jugyo <jugyo.org@gmail.com>
2009-04-16 22:32:49 +0900

    Enable for :reply and :direct.

--- lib/plugins/confirm.rb
+++ lib/plugins/confirm.rb
@@ -1,12 +1,33 @@
 # -*- coding: utf-8 -*-
 
+config.plugins.confirm.set_default(:commands, [:update, :reply, :direct])
+config.plugins.confirm.set_default(
+  :conditions,
+  [
+    lambda { |cmd_name, arg|
+      if cmd_name == :direct && arg =~ /^(list|sent_list)$/
+        false
+      else
+        true
+      end
+    }
+  ]
+)
+
 Termtter::Client.register_hook(
   :name => :confirm,
-  :points => [:pre_exec_update],
+  :points => [/^pre_exec_/],
   :exec_proc => lambda {|cmd, arg|
-    if /^y?$/i !~ Readline.readline("update? #{arg} [Y/n] ", false)
-      puts 'canceled.'
-      raise Termtter::CommandCanceled
+    if config.plugins.confirm.commands.include?(cmd.name) && 
+        config.plugins.confirm.conditions.any? { |cond|  cond.call(cmd.name, arg) }
+
+      prompt = "\"#{cmd.name} #{arg}".strip + "\" [Y/n] "
+
+      if /^y?$/i !~ Readline.readline(prompt, false)
+        puts 'canceled.'
+        raise Termtter::CommandCanceled
+      end
+
     end
   }
 )
