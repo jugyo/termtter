28f5301d14eb8c5ad9a0af66c9f6ad6ea4379f53
jugyo <jugyo.org@gmail.com>
2009-04-26 23:21:07 +0900

    Changed to warn when use method plugin.

--- lib/termtter/system_extensions.rb
+++ lib/termtter/system_extensions.rb
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 def plugin(name, init = {})
+  warn "plugin method will be removed. Use Termtter::Client.plug instead."
   unless init.empty?
     init.each do |key, value|
       config.plugins.__refer__(name.to_sym).__assign__(key.to_sym, value)
