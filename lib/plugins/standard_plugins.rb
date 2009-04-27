8423f40d62c3321b342e320fb7ffe81dfa9c3594
jugyo <jugyo.org@gmail.com>
2009-04-27 09:28:01 +0900

    Moved _update_timeline to standard_plugins.rb

--- lib/plugins/standard_plugins.rb
+++ lib/plugins/standard_plugins.rb
@@ -10,6 +10,30 @@ config.plugins.standard.set_default(
 module Termtter::Client
 
   register_command(
+    :name => :_update_timeline,
+    :exec_proc => lambda {|arg|
+      begin
+        args = @since_id ? [{:since_id => @since_id}] : []
+        statuses = Termtter::API.twitter.friends_timeline(*args)
+        unless statuses.empty?
+          print "\e[1K\e[0G" unless win?
+          @since_id = statuses[0].id
+          output(statuses, :update_friends_timeline)
+          Readline.refresh_line
+        end
+      rescue OpenURI::HTTPError => e
+        if e.message == '401 Unauthorized'
+          puts 'Could not login'
+          puts 'plese check your account settings'
+          exit!
+        end
+      rescue => e
+        handle_error(e)
+      end
+    }
+  )
+
+  register_command(
     :name => :update, :aliases => [:u],
     :exec_proc => lambda {|arg|
       unless arg.empty?
