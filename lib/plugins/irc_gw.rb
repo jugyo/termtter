28cd848f3e7ab96508520de72607667c639a7fcc
jugyo <jugyo.org@gmail.com>
2009-04-29 22:38:51 +0900

    Changed to use channel.

--- lib/plugins/irc_gw.rb
+++ lib/plugins/irc_gw.rb
@@ -8,32 +8,31 @@ require 'net/irc'
 config.plugins.irc_gw.set_default(:port, 16669)
 
 class TermtterIrcGateway < Net::IRC::Server::Session
-  attr_reader :server_name, :server_version
+  def server_name; 'termtter' end
+  def server_version; '0.0.0' end
+  def main_channel; '#termtter' end
+
   def initialize(*args)
     super
-    @server_name = 'termttergw'
-    @server_version = '0.0.0'
     Termtter::Client.register_hook(
       :name => :irc_gw,
       :point => :output,
       :exec => lambda { |statuses, event|
         statuses.each do |s|
-          post s.user.screen_name, PRIVMSG, '_', s.text
+          post s.user.screen_name, PRIVMSG, main_channel, s.text
         end
       }
     )
   end
 
-  def on_message(m)
-    if @user
-      begin
-        # TODO: ここの処理、微妙です。on_message にはいろんな種類のメッセージが来るんです。。
-        termtter_command = m.command.downcase + ' ' + m.params.join(' ')
-        Termtter::Client.call_commands(termtter_command)
-      rescue => e
-        Termtter::Client.handle_error e
-      end
-    end
+  def on_user(m)
+    super
+    post @prefix, JOIN, main_channel
+  end
+
+  def on_privmsg(m)
+    target, message = *m.params
+    Termtter::Client.call_commands('update ' + message)
   end
 end
 
