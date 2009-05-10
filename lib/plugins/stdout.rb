044481f97c870c9769b6deabc7648bf49a25dd0f
jugyo <jugyo.org@gmail.com>
2009-05-11 00:28:00 +0900

    Fixed for typable id.
    該当するデータがなかったときは変換しないようにした。

--- lib/plugins/stdout.rb
+++ lib/plugins/stdout.rb
@@ -120,7 +120,7 @@ module Termtter
       if arg
         prefix = config.plugins.stdout.typable_id_prefix
         arg.gsub(/#{Regexp.quote(prefix)}\w+/) do |id|
-          Termtter::Client.typable_id_to_data(id[1..-1])
+          Termtter::Client.typable_id_to_data(id[1..-1]) || id
         end
       else
         arg
