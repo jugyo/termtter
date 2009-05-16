aea613b40ee0fc7f87391f91a14d578277c15e4c
emasaka <emasaka@gmail.com>
2009-05-16 11:17:06 +0900

    use multiple URL shortening APIs.
    for Failover.

--- lib/plugins/tinyurl.rb
+++ lib/plugins/tinyurl.rb
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+SHORTURL_MAKERS = [
+  { :host => "tinyurl.com", :format => '/api-create.php?url=%s' },
+  { :host => "is.gd", :format => '/api.php?longurl=%s' },
+]
 TINYURL_HOOK_COMMANDS = [:update, :reply]
 URI_REGEXP = URI.regexp(%w(http https ftp))
 
@@ -10,9 +14,19 @@ Termtter::Client.register_hook(
   },
   :exec_proc => lambda {|cmd, arg|
     arg = arg.gsub(URI_REGEXP) do |url|
-      Termtter::API.connection.start('tinyurl.com', 80) do |http|
-        http.get('/api-create.php?url=' + URI.escape(url)).body
+      url_enc = URI.escape(url)
+      result = url
+      SHORTURL_MAKERS.each do |site|
+        res = nil
+        Termtter::API.connection.start(site[:host], 80) do |http|
+          res = http.get(site[:format] % url_enc)
+        end
+        if res.code == '200'
+          result = res.body
+          break
+        end
       end
+      result
     end
   }
 )
