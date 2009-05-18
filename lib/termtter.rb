4e6296fad0c87d14cd109f2795b8bce7cf65f91d
jugyo <jugyo.org@gmail.com>
2009-05-19 00:10:42 +0900

    Changed to use rb-readline!

--- lib/termtter.rb
+++ lib/termtter.rb
@@ -5,6 +5,8 @@ $KCODE="u" unless Object.const_defined? :Encoding
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+$:.unshift(File.dirname(__FILE__) + '/../vendor/rb-readline/readline/lib')
+
 require 'rubygems'
 require 'json'
 require 'net/https'
