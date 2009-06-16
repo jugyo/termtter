0c8ee0dc1fd2595578ec1b771a5686c536e6b454
jugyo <jugyo.org@gmail.com>
2009-06-16 22:23:58 +0900

    Changed to handle files

--- lib/plugins/http_server.rb
+++ lib/plugins/http_server.rb
@@ -1,3 +1,11 @@
+# http_server
+#
+# = REQUIREMENTS
+# * mime-types
+#
+
+require 'mime/types'
+require 'pathname'
 require 'webrick'
 require 'logger'
 
@@ -25,16 +33,27 @@ module Termtter::Client
     :AccessLog => []
   )
 
-  @http_server.mount_proc('/execute') do |req, res|
-    @http_server_output = ''
-    begin
-      command = req.path.sub(/^\/execute\//, '')
-      call_commands(command)
-      res['Content-Type'] = 'text/javascript; charset=utf-8';
-      res.body = @http_server_output
-    rescue Termtter::CommandNotFound => e
-      res.status = 404
-      res.body = "Command Not Found!!"
+  @http_server.mount_proc('/') do |req, res|
+    request_path = req.path == '/' ? 'index.html' : req.path
+    base_name = Pathname.new(request_path).basename.to_s
+    file_path = File.dirname(__FILE__) + '/http_server/' + base_name
+
+    if File.file?(file_path)
+      # send a file
+      res.header["Content-Type"] = MIME::Types.type_for(file_path).first.content_type
+      res.body = File.open(file_path, 'rb').read
+    else
+      # execute a command
+      @http_server_output = ''
+      begin
+        command = req.path.sub(/^\//, '')
+        call_commands(command)
+        res['Content-Type'] = 'text/javascript; charset=utf-8';
+        res.body = @http_server_output
+      rescue Termtter::CommandNotFound => e
+        res.status = 404
+        res.body = "Command Not Found!!"
+      end
     end
   end
 
