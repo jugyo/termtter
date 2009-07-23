5a4d2b47ffcf5df33fae9c6db7abb3fcac82c073
jugyo <kkohno@gmail.com>
2009-07-23 18:06:15 +0900

    Deleted useless codes.

--- lib/plugins/shell.rb
+++ lib/plugins/shell.rb
@@ -4,11 +4,6 @@ module Termtter::Client
   register_command :name => :shell, :aliases => [:sh],
     :help => ['shell,sh', 'Start your shell'],
     :exec_proc => lambda {|args|
-      begin
-        pause
-        system ENV['SHELL'] || ENV['COMSPEC']
-      ensure
-        resume
-      end
+      system ENV['SHELL'] || ENV['COMSPEC']
     }
 end
