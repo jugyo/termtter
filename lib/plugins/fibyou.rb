daee2bb68313ac108277ce35f61ea6e948f67110
jugyo <jugyo.org@gmail.com>
2009-08-12 00:03:08 +0900

    Separated plugin as fib and fibyou

---
+++ lib/plugins/fibyou.rb
@@ -0,0 +1,12 @@
+# Requirements: fib plugin
+
+module Termtter::Client
+  register_command(:fibyou) do |arg|
+    /(\w+)\s(\d+)/ =~ arg
+    name = normalize_as_user_name($1)
+    n = $2.to_i
+    text = "@#{name} fib(#{n}) = #{fib n}"
+    Termtter::API.twitter.update(text)
+    puts "=> " << text
+  end
+end
