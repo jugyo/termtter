dcafed55da3d472663233cac2e70502d10f15188
Koichiro Ohba <koichiro@meadowy.org>
2009-08-18 21:24:30 +0900

    Added plugin for quick_exit.rb

---
+++ lib/plugins/quick_exit.rb
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+#config.plugins.alias.aliases = { :e => 'exit', :q => 'exit'}
+#
+ 
+module Termtter::Client
+  register_alias(:e, 'exit')
+  register_alias(:q, 'exit')
+end
