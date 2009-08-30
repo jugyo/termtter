2310bb2a49e90fbc73d4a443fd42a59250e87aa5
emasaka <emasaka@gmail.com>
2009-08-30 14:28:37 +0900

    Set Readline.completion_case_fold to true.
    Some completion procs (e.g. hashtags) return list ignoring letter case.
    But if completion_case_fold is false, and upper/lower case is mixed in list,
    the word will be cleaned from input string.

--- lib/plugins/defaults/command_line.rb
+++ lib/plugins/defaults/command_line.rb
@@ -64,6 +64,7 @@ module Termtter
       if Readline.respond_to?(:basic_word_break_characters=)
         Readline.basic_word_break_characters= "\t\n\"\\'`><=;|&{("
       end
+      Readline.completion_case_fold = true
       Readline.completion_proc = lambda {|input|
         begin
           words = []
