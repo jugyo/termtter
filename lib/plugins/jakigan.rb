522bb27452e367ff68e444f01e2cfe58d81bac30
ujihisa <ujihisa@gmail.com>
2009-09-02 10:56:11 -0700

    Fix

--- lib/plugins/jakigan.rb
+++ lib/plugins/jakigan.rb
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
+
+if RUBY_VERSION >= '1.9'
+  # nop
+elsif RUBY_VERSION >= '1.8.7'
+  class Array
+    alias sample choice
+  end
+else
+  class Array
+    def sample
+      at(rand(size))
+    end
+  end
+end
+
 module Termtter::Client
-  register_macro(:jkg, "update %s、か。ククク、その真の意図、邪気眼を持たぬ者には分かるまい。",
+  module Jakigan
+    TEMPLATES = ['%s、か。ククク、その真の意図、邪気眼を持たぬ者には分かるまい。']
+  end
+  register_macro(:jkg, "update #{Jakigan::TEMPLATES.sample}",
     :help => ['jkg {MESSAGE}', 'update "{MESSAGE}+something jkg."'],
     :aliases => [:j, :jk]
   )
