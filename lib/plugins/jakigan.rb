4645110b8742b327a0d4873c1b724aca5e1c05fb
ujihisa <ujihisa@gmail.com>
2009-09-02 18:28:11 -0700

    Removed j alias

--- lib/plugins/jakigan.rb
+++ lib/plugins/jakigan.rb
@@ -2,6 +2,6 @@
 module Termtter::Client
   register_macro(:jkg, "update %s、か。ククク、その真の意図、邪気眼を持たぬ者には分かるまい。",
     :help => ['jkg {MESSAGE}', 'update "{MESSAGE}+something jkg."'],
-    :aliases => [:j, :jk]
+    :alias => :jk
   )
 end
