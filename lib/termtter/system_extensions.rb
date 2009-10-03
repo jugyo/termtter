ca0bd0981adacfba67b98071c36ab042790051ea
hitode909 <hitode909@gmail.com>
2009-10-03 17:33:59 +0900

    delete def plugin, filter(deprecated)

--- lib/termtter/system_extensions.rb
+++ lib/termtter/system_extensions.rb
@@ -1,22 +1,5 @@
 # -*- coding: utf-8 -*-
 
-def plugin(name, init = {})
-  warn "plugin method will be removed. Use Termtter::Client.plug instead."
-  unless init.empty?
-    init.each do |key, value|
-      config.plugins.__refer__(name.to_sym).__assign__(key.to_sym, value)
-    end
-  end
-  load "plugins/#{name}.rb"
-rescue Exception => e
-  Termtter::Client.handle_error(e)
-end
-
-def filter(name, init = {})
-  warn "filter method will be removed. Use plugin instead."
-  plugin(name, init)
-end
-
 require 'dl/import'
 module Readline
   begin
