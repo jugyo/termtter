324ac761a907976458aeced84d46ee8162520b1d
Tomohiro Nishimura <tomohiro68@gmail.com>
2009-10-04 18:29:08 +0900

    add test for Readline::LIBREADLINE
      * and we got a 100% coverage in core system!!!

--- spec/termtter/system_extensions_spec.rb
+++ spec/termtter/system_extensions_spec.rb
@@ -18,5 +18,24 @@ describe Termtter do
     win?.should == true
     RUBY_PLATFORM = original_ruby_platform
   end
+
+  it 'Readline can refresh line' do
+    Readline::LIBREADLINE.should_receive(:rl_refresh_line).with(0, 0)
+    Readline.refresh_line
+  end
+
+  it 'extend DL::Impoter when not be able to find DL::Importable' do
+    DL::Importer = mock(:importer)
+    DL.stub(:const_defined?).with(:Importable).and_return(false)
+    Readline::LIBREADLINE.should_receive(:extend).with(DL::Importer)
+    load 'termtter/system_extensions.rb'
+  end
+
+  it 'can handle error when difine LIBREADLINE' do
+    Readline::LIBREADLINE.stub(:extend) { raise }
+    load 'termtter/system_extensions.rb'
+    Readline::LIBREADLINE.should_not_receive(:rl_refresh_line)
+    Readline.refresh_line
+  end
 end
 
