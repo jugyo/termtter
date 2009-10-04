10b2e584f8f95df7172b65626a8ac1bde7717af4
Tomohiro Nishimura <tomohiro68@gmail.com>
2009-10-04 17:02:38 +0900

    delete support for old version ruby

--- lib/termtter/system_extensions.rb
+++ lib/termtter/system_extensions.rb
@@ -116,20 +116,6 @@ if win?
   end
 end
 
-unless Array.instance_methods.include?('take')
-  class Array
-    def take(n) self[0...n] end
-  end
-end
-
-unless Symbol.instance_methods.include?('to_proc')
-  class Symbol
-    def to_proc
-      Proc.new { |*args| args.shift.__send__(self, *args) }
-    end
-  end
-end
-
 require 'highline'
 def create_highline
   HighLine.track_eof = false
