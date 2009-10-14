652b721fe6cebad638557af4ac84de49a37bc900
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2009-10-14 09:26:39 +0900

    Fix unnecessary post to wassr if you cancel confirmation.
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard+git@gmail.com>

--- lib/plugins/defaults/retweet.rb
+++ lib/plugins/defaults/retweet.rb
@@ -13,6 +13,8 @@ module Termtter::Client
     text = ERB.new(config.plugins.retweet.format).result(binding)
     Termtter::API.twitter.update(text)
     puts "=> #{text}"
+
+    return text
   end
 
   register_command(
@@ -21,19 +23,21 @@ module Termtter::Client
     :help      => ['retweet,rt (ID|@USER)', 'Post a retweet message'],
     :exec_proc => lambda {|arg|
       arg, comment = arg.split(/\s/, 2)
-      if public_storage[:typable_id] && s = typable_id_status(arg)
-        post_retweet(s, comment)
-      else
-        case arg
-        when /(\d+)/
-          post_retweet(Termtter::API.twitter.show(arg), comment)
-        when /@([A-Za-z0-9_]+)/
-          user = $1
-          statuses = Termtter::API.twitter.user_timeline(user)
-          return if statuses.empty?
-          post_retweet(statuses[0], comment)
-        end
-      end
+      txt = if public_storage[:typable_id] && s = typable_id_status(arg)
+              post_retweet(s, comment)
+            else
+              case arg
+              when /(\d+)/
+                post_retweet(Termtter::API.twitter.show(arg), comment)
+              when /@([A-Za-z0-9_]+)/
+                user = $1
+                statuses = Termtter::API.twitter.user_timeline(user)
+                return if statuses.empty?
+                post_retweet(statuses[0], comment)
+              end
+            end
+
+      return txt
     }
   )
 end
--- lib/plugins/multi_post.rb
+++ lib/plugins/multi_post.rb
@@ -3,30 +3,45 @@
 module Termtter::Client
   class << self
     def wassr_update(text)
+      if text.match(/^(\d+)\s+(.+)$/) and
+          (s = Termtter::API.twitter.show($1) rescue nil)
+        tmp_text = "@#{s.user.screen_name} #{$2}"
+      else
+        tmp_text = text
+      end
+
       Net::HTTP.version_1_2
       req = Net::HTTP::Post.new("/statuses/update.json?")
       req.basic_auth config.plugins.wassr.username, config.plugins.wassr.password
       Net::HTTP.start('api.wassr.jp', 80) do |http|
-        res = http.request(req, "status=#{URI.escape(text)}&source=Termtter")
+        res = http.request(req, "status=#{URI.escape(tmp_text)}&source=Termtter")
       end
     end
   end
+
+  register_hook(
+    :name => :multi_post,
+    :points => [:post_exec_update, :post_exec_reply, :post_exec_retweet],
+    :exec_proc => lambda {|cmd, arg, result|
+      prefix = config.plugins.stdout.typable_id_prefix
+      if result
+        wassr_arg = result
+      elsif arg.match(/(\$([a-z]{2}))/) and
+          s = Termtter::API.twitter.show(typable_id_to_data($2))
+        wassr_arg = arg.sub($1, "@" + s.user.screen_name)
+      else
+        wassr_arg = arg
+      end
+
+      begin
+        Termtter::Client.wassr_update(wassr_arg.strip)
+      rescue
+        puts "RuntimeError: #{$!}"
+      end
+    }
+  )
 end
 
-Termtter::Client.register_hook(
-  :name => :multi_post,
-  :points => [:modify_arg_for_update, :modify_arg_for_reply],
-  :exec_proc => lambda {|cmd, arg|
-    begin
-      wassr_arg = arg.gsub(/\d{10,}/, '')
-      Termtter::Client.wassr_update(wassr_arg.strip)
-    rescue
-      puts "RuntimeError: #{$!}"
-    end
-
-    return arg
-  }
-)
 
 # multi_post.rb
 # One post, multi update.
