1e8b6326377549ffd0fe7ead91df69c45e6b0ebd
Tomohiro Nishimura <tomohiro68@gmail.com>
2009-11-03 12:45:01 +0900

    support DB plugin (stream plugin)
      * support BD plugin
      * add stop_stream command

--- lib/plugins/stream.rb
+++ lib/plugins/stream.rb
@@ -7,17 +7,25 @@ require 'lib/termtter/active_rubytter'
 module Termtter::Client
 
   class << self
-    def friends(max = 1/0.0)
-      user_name = config.user_name
-
-      friends = []
-      page = 0
-      begin
-        friends += tmp = Termtter::API::twitter.friends(user_name, :page => page+=1)
-        p friends.length
-      rescue
-      end until (tmp.empty? or friends.length > max)
-      friends.take(max)
+    if defined?(DB)
+      def friends(max = 1/0.0)
+        Status.group(:user_id).
+          select(:user_id, :screen_name).
+          join(:users, :id => :user_id).
+          order(:COUNT.sql_function.desc).
+          take(max)
+      end
+    else
+      def friends(max = 1/0.0)
+        friends = []
+        page    = 0
+        begin
+          friends += tmp = Termtter::API::twitter.friends(config.user_name, :page => page+=1)
+          p friends.length
+        rescue
+        end until (tmp.empty? or friends.length > max)
+        friends.take(max)
+      end
     end
   end
 
@@ -28,12 +36,25 @@ module Termtter::Client
     end
 
     if targets.empty?
-      targets = friends(370)
-      p targets.map(&:screen_name)
-      targets = targets.map(&:id)
+      if defined?(DB)
+        names = []
+        friends(370).each do |t|
+          names << t[:screen_name]
+          targets << t.user_id
+        end
+        p names
+      else
+        names = []
+        friends(370).each do |t|
+          names << t.screen_name
+          targets << t.id
+        end
+        p names
+      end
     end
 
-    stream = Thread.new do
+
+    config.plugins.stream.thread = Thread.new do
       TweetStream::Client.new(config.user_name, config.password).
         filter(:follow => targets) do |status|
           output [Termtter::ActiveRubytter.new(status)], :stream_output
@@ -41,8 +62,12 @@ module Termtter::Client
     end
 
     at_exit do
-      stream.kill
+      config.plugins.stream.thread.kill
     end
+  end
+
+  register_command(:stop_stream) do |args|
+    config.plugins.stream.thread.kill
   end
 end
 
