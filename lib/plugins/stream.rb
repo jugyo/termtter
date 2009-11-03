feb4d87e6c88cec91e7c4b147951edcb1bffddd4
hitode909 <hitode909@gmail.com>
2009-11-03 12:53:22 +0900

    streamのfollow数調整

--- lib/plugins/stream.rb
+++ lib/plugins/stream.rb
@@ -4,40 +4,33 @@ require 'uri'
 require 'tweetstream'
 require 'lib/termtter/active_rubytter'
 
+config.plugins.stream.set_default :max_following, 400
+
 module Termtter::Client
 
   class << self
     def friends(max = 1/0.0)
-      user_name = config.user_name
-
-      friends = []
-      page = 0
-      begin
-        friends += tmp = Termtter::API::twitter.friends(user_name, :page => page+=1)
-        p friends.length
-      rescue
-      end until (tmp.empty? or friends.length > max)
-      friends.take(max)
+      Users.take(max).map{ |u| u.id}
     end
   end
 
   register_command(:stream) do |arg|
 
-    targets = arg.split.map do |name|
-      Termtter::API.twitter.user(name).id
-    end
-
-    if targets.empty?
-      targets = friends(370)
-      p targets.map(&:screen_name)
-      targets = targets.map(&:id)
-    end
-
     stream = Thread.new do
-      TweetStream::Client.new(config.user_name, config.password).
-        filter(:follow => targets) do |status|
+      begin
+        max = config.plugins.stream.max_following
+        puts "streaming #{max} friends."
+        p User.take(max).map(&:id)
+        TweetStream::Client.new(config.user_name, config.password).
+          filter(:follow => User.take(max).map(&:id)) do |status|
           output [Termtter::ActiveRubytter.new(status)], :stream_output
         end
+      rescue => e
+        p e
+        puts "streaming seems broken."
+        config.plugins.stream.max_following -= 10 if config.plugins.stream.max_following > 10
+        retry
+      end
     end
 
     at_exit do
