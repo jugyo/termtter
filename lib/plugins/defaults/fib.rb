224748f24d8b1f8b366a4fe08d560c82223d8536
OHASHI Hideya <ohachige@gmail.com>
2009-11-08 16:34:04 +0900

    plugin fib: refactoring

--- lib/plugins/defaults/fib.rb
+++ lib/plugins/defaults/fib.rb
@@ -5,14 +5,17 @@ def fib(n)
   m.nil? ? fibsub(n,0,1) : fibsub(n-m[0],m[1][0],m[1][1])
 end
 step=100
-@fibs[step] = [fib(step), fib(step+1)]
-(2..6).each do |i|
+(1..6).each do |i|
   j=i*step
   @fibs[j] = [fib(j), fib(j+1)]
 end
 Termtter::Client.register_command(:fib) do |arg|
-  n = arg.to_i # up to 618
-  text = "fib(#{n}) = #{fib n}"
-  Termtter::API.twitter.update(text)
-  puts "=> " << text
+  n = arg.to_i
+  if n > 618
+    puts "=> too big"
+  else
+    text = "fib(#{n}) = #{fib n}"
+    Termtter::API.twitter.update(text)
+    puts "=> " << text
+  end
 end
