bd0f7df350ebf3e4ab8c45efb6de02949ea3d1d9
jugyo <jugyo.org@gmail.com>
2009-11-15 09:45:20 +0900

    save password (on memory) for switch user

--- lib/plugins/defaults/switch.rb
+++ lib/plugins/defaults/switch.rb
@@ -1,15 +1,29 @@
 module Termtter::Client
+  passwords = {}
+
   register_command(
     :name => :switch,
     :alias => :switch_user,
     :exec_proc => lambda {|arg|
-      if arg.empty?
-        config.__clear__(:user_name)
-      else
+      user_name = !arg.empty? ? normalize_as_user_name(arg) : nil
+      return if user_name == config.user_name
+
+      passwords[config.user_name] = config.password
+
+      if user_name
         config.user_name = normalize_as_user_name(arg)
+        if passwords.key?(config.user_name)
+          config.password = passwords[config.user_name]
+        else
+          config.__clear__(:password)
+        end
+      else
+        config.__clear__(:user_name)
+        config.__clear__(:password)
       end
-      config.__clear__(:password)
+
       Termtter::API.setup
+      call_commands('reload')
     },
     :help => ["switch USERNAME", "Switch twitter account."]
   )
