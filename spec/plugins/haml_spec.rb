a19938fe4d97ca18a4b57a9439a87bb41d871d47
Keita Urashima <ursm@ursm.jp>
2009-11-17 03:01:35 +0900

    fixed junk spec

--- spec/plugins/haml_spec.rb
+++ spec/plugins/haml_spec.rb
@@ -103,7 +103,9 @@ describe Termtter::Plugins::Haml do
       end
 
       it 'gives priority to argument' do
-        subject.haml('html5').should == '<!DOCTYPE html>'
+        subject.haml('xhtml').should == <<-DTD.chomp
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        DTD
       end
     end
 
