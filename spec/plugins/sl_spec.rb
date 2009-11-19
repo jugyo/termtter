c52a1aa79b51c414d8f74ec957c941219ffa7ab9
Tomohiro Nishimura <tomohiro68@gmail.com>
2009-11-19 18:24:14 +0900

    remove old plugins spec (xxx_spec_.rb is backup)

--- spec/plugins/sl_spec.rb
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-require File.dirname(__FILE__) + '/../spec_helper'
-describe Termtter do
-  it 'plugin sl' do
-    Termtter::Client.should_receive(:register_command).exactly(4).times
-    Termtter::Client.plug 'sl'
-  end
-end
