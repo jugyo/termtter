3dcf18c67eede5a9c500e8e3bfc1e82fc721581a
Tomohiro Nishimura <tomohiro68@gmail.com>
2009-11-25 05:27:07 +0900

    add favotter_fav command to favotter plugin

--- lib/plugins/favotter.rb
+++ lib/plugins/favotter.rb
@@ -6,15 +6,21 @@ require 'open-uri'
 
 module Termtter::Client
 
+  public_storage[:favorited_ids] = {}
+
   class << self
     def output_favorites(target, threshold)
       url = "http://favotter.matope.com/user.php?user=#{target}&threshold=#{threshold}"
-      parse(url).reverse.each do |text, amount, users|
+      public_storage[:favorited_ids].clear
+      alphabet = '$a'
+      parse(url).reverse.each do |id, text, amount, users|
+        public_storage[:favorited_ids][alphabet] = id
         color = fav_color(amount)
         fav = "fav#{amount == 1 ? '' : 's'}".rjust(4)
-        format = "<GREEN>%s #{fav} by</GREEN> <YELLOW>%s</YELLOW>: <#{color}>%s</#{color}>"
+        format = "#{alphabet}<GREEN>%s #{fav} by</GREEN> <YELLOW>%s</YELLOW>: <#{color}>%s</#{color}>"
         values = [amount.to_s.rjust(3), users.join(', '), CGI.escape(text)]
         puts CGI.unescape(TermColor.parse(format % values ))
+        alphabet.succ!
       end
     end
 
@@ -22,11 +28,12 @@ module Termtter::Client
     def parse(url)
       doc = Nokogiri(open(url).read)
       doc.css('div.entry').map do |entry|
+        id     = entry['id'].gsub(/\Astatus_/, '')
         text   = entry.css('span.status_text').first.content
         amount = entry.css('div.info span.favotters').first.content
         amount = amount.match(/(\d+)/)[1].to_i
         users  = entry.css('div.info span.favotters img').map {|u| u['title'] }
-        [text, amount, users]
+        [id, text, amount, users]
       end
     end
 
@@ -55,4 +62,13 @@ module Termtter::Client
     end
     output_favorites target, threshold
   end
+
+  help = ['favotter_fav [FavoritedID]', 'Favorite favorited status']
+  register_command('favotter_fav', :alias => :ffav, :help => help) do |arg|
+    raise 'need favorited_id' if arg.empty?
+    if id = public_storage[:favorited_ids][arg]
+      call_commands("favorite #{id}")
+    end
+  end
 end
+
