2339fd7afe4dd3047563e961a2a41d1ca72e7ae1
Tomohiro Nishimura <tomohiro68@gmail.com>
2009-11-25 00:31:22 +0900

    improved favotter plugin

--- lib/plugins/favotter.rb
+++ lib/plugins/favotter.rb
@@ -7,10 +7,14 @@ require 'open-uri'
 module Termtter::Client
 
   class << self
-    def output_favorites(target)
-      url = "http://favotter.matope.com/user.php?user=#{target}"
+    def output_favorites(target, threshold)
+      url = "http://favotter.matope.com/user.php?user=#{target}&threshold=#{threshold}"
       parse(url).reverse.each do |text, amount, users|
-        puts TermColor.parse("#{fav_color(amount.rjust(10))} <YELLOW>#{users.join(', ')}</YELLOW>:「#{text}」")
+        color = fav_color(amount)
+        fav = "fav#{amount == 1 ? '' : 's'}".rjust(4)
+        format = "<GREEN>%s #{fav} by</GREEN> <YELLOW>%s</YELLOW>: <#{color}>%s</#{color}>"
+        values = [amount.to_s.rjust(3), users.join(', '), CGI.escape(text)]
+        puts CGI.unescape(TermColor.parse(format % values ))
       end
     end
 
@@ -20,30 +24,35 @@ module Termtter::Client
       doc.css('div.entry').map do |entry|
         text   = entry.css('span.status_text').first.content
         amount = entry.css('div.info span.favotters').first.content
+        amount = amount.match(/(\d)/)[1].to_i
         users  = entry.css('div.info span.favotters img').map {|u| u['title'] }
         [text, amount, users]
       end
     end
 
     def fav_color(amount)
-      num = amount.match(/(\d)/)[1].to_i
-      return amount if num == 1
-      color = case num
+      case amount
+        when 1 then 'WHITE'
         when 2 then 'GREEN'
         when 3 then 'BLUE'
         when 4 then 'BLUE'
         else        'RED'
       end
-      "<#{color}>#{amount}</#{color}>"
     end
   end
 
-  help = ['favotter [USERNAME]', 'Show info from favotter']
-  register_command('favotter', :help => help) do |args|
-    target = args.empty? ? config.user_name : args
+  help = ['favotter [USERNAME] [THRESHOLD]', 'Show info from favotter']
+  register_command('favotter', :help => help) do |arg|
+    target = if arg.empty?
+       config.user_name
+     else
+       args = arg.split
+       threshold = args.size == 1 ? 1 : args[1]
+       args[0]
+    end
     if /@(.*)/ =~ target
       target = $1
     end
-    output_favorites target
+    output_favorites target, threshold
   end
 end
