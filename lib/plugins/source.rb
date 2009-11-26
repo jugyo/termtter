1c513296cdd5f3a05caea5823306a55fa8f65045
ujihisa <ujihisa@gmail.com>
2009-11-25 21:13:33 -0800

    Refactoring (without tests)

--- lib/plugins/source.rb
+++ lib/plugins/source.rb
@@ -3,14 +3,20 @@ def __dir__
   File.dirname(File.expand_path(__FILE__))
 end
 
-def source(pluginname)
-  a = Pathname(__dir__) + "#{pluginname}.rb"
-  unless File.exist?(a)
-    a = Pathname(__dir__) + "defaults/#{pluginname}.rb"
-  end
-  File.read(a)
+def let(o)
+  yield o
 end
 
+def source(pluginname)
+  File.read(
+    let(
+      Pathname(__dir__) + "#{pluginname}.rb") {|a|
+        File.exist?(a) ? a : Pathname(__dir__) + "defaults/#{pluginname}.rb" })
+end
+
+def truncate(s)
+  s.each_char.take(140).join
+end
 
 module Termtter::Client
   register_command(:source, :help => ["source {plugin-name}", "shows the source code of the plugin"]) do |arg|
@@ -19,7 +25,7 @@ module Termtter::Client
 
   register_command(:sourceyou, :help => ["sourceyou @username {plugin-name}", "gives the source code of the plugin"]) do |arg|
     /(\w+)\s(\w+)/ =~ arg
-    text = "@#{normalize_as_user_name($1)} #{source($2)}".each_char.take(140).join
+    text = truncate("@#{normalize_as_user_name($1)} #{source($2)}")
     Termtter::API.twitter.update(text)
   end
 end
