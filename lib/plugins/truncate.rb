021697859d1f6c396d34297f61ea4a8223d32623
Koichiro Ohba <koichiro@meadowy.org>
2009-11-27 00:57:43 +0900

    Use RubytterProxy#post_update hook in truncate.

--- lib/plugins/truncate.rb
+++ lib/plugins/truncate.rb
@@ -11,22 +11,19 @@ def truncate(text, length = 140, omission = "...")
   chars.length > length ? (chars[0...l] + o).pack('U*') : text
 end
 
-unless Object.const_defined?(:TRUNCATE_HOOK_COMMANDS)
-  TRUNCATE_HOOK_COMMANDS = [:update, :reply, :retweet]
-end
-
-Termtter::Client::register_hook(
+Termtter::RubytterProxy.register_hook(
   :name => :truncate_status,
-  :points => TRUNCATE_HOOK_COMMANDS.map {|cmd|
-    "modify_arg_for_#{cmd.to_s}".to_sym
-  },
-  :exec_proc => lambda do |cmd, arg|
-    return arg if multibyte_string(arg).length <= 140
-    if Termtter::Client::confirm("You are status contents more than 140 characters. Do you want abbreviation status?", true)
-      truncate(arg)
+  :points => [:pre_update],
+  :exec_proc => lambda do |*args|
+    return unless args
+    return args if args.length < 1
+    status = args[0]
+    return args if multibyte_string(status).length <= 140
+    if Termtter::Client::confirm("You are status contents more than 140 characters. do end endyou want abbreviation status?", true)
+      args[0] = truncate(status)
     else
       puts 'canceled.'
-      raise Termtter::CommandCanceled
+      raise Termtter::HookCanceled
     end
   end
 )
