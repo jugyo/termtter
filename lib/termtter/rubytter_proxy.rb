16517d174180b979fbdabbde7eba36723117531f
jugyo <jugyo.org@gmail.com>
2009-12-05 08:36:56 +0900

    RubytterProxy supports retry.

--- lib/termtter/rubytter_proxy.rb
+++ lib/termtter/rubytter_proxy.rb
@@ -16,9 +16,18 @@ module Termtter
             modified_args = hook.call(*modified_args)
           end
 
-          timeout(config.timeout) do
-            result = @rubytter.__send__(method, *modified_args)
+          success = false
+          config.retry.times.each do
+            begin
+              timeout(config.timeout) do
+                result = @rubytter.__send__(method, *modified_args)
+              end
+              success = true
+              break
+            rescue TimeoutError
+            end
           end
+          raise TimeoutError, 'execution expired' unless success
 
           self.class.call_hooks("post_#{method}", *args)
         rescue HookCanceled
