bcf3516cd59ed56599f09f27b9f5f577a94b36bb
jugyo <jugyo.org@gmail.com>
2009-12-05 17:15:30 +0900

    refactoring

--- lib/plugins/g.rb
+++ lib/plugins/g.rb
@@ -1,16 +1,9 @@
-module Termtter::Client
-  begin
-    require 'g'
-    register_command(
-      :name      => :g,
-      :help      => ['g obj', "Do you know 'g'? It's like 'p'."],
-      :exec_proc => lambda {|arg|
-        # we get arg as String, so without eval() it is not very useful
-        # but we shouldn't blindly eval user-supplied string like this, either
-        g eval(arg)
-      }
-    )
-  rescue e
-    handle_error(e)
-  end
+require 'g'
+Termtter::Client.register_command(
+  :g,
+  :help => ['g OBJECT', "Do you know 'g'? It's like 'p'."]
+) do |arg|
+  # we get arg as String, so without eval() it is not very useful
+  # but we shouldn't blindly eval user-supplied string like this, either
+  g eval(arg)
 end
