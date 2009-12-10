a1918bd517dfc6cf83d002f3afb841f9f97eacc6
jugyo <jugyo.org@gmail.com>
2009-12-11 00:22:12 +0900

    'confirm' supports 'direct_message'

--- lib/plugins/defaults/confirm.rb
+++ lib/plugins/defaults/confirm.rb
@@ -7,5 +7,12 @@ module Termtter
     end
     args
   end
+
+  RubytterProxy.register_hook(:confirm, :point => :pre_direct_message) do |*args|
+    if config.confirm && !Client.confirm("direct #{args[0]} #{args[1]}")
+      raise CommandCanceled
+    end
+    args
+  end
 end
 
