5315dbb49b917701b114744da038cbe528c35c5c
jugyo <jugyo.org@gmail.com>
2009-12-10 18:42:13 -0700

    fix bug for confirm

--- lib/plugins/defaults/confirm.rb
+++ lib/plugins/defaults/confirm.rb
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 config.set_default(:confirm, false)
 module Termtter
-  RubytterProxy.register_hook(:confirm, :point => :pre_update) do |*args|
+  RubytterProxy.register_hook(:confirm_update, :point => :pre_update) do |*args|
     if config.confirm && !Client.confirm("update #{args.first}")
       raise CommandCanceled
     end
     args
   end
 
-  RubytterProxy.register_hook(:confirm, :point => :pre_direct_message) do |*args|
+  RubytterProxy.register_hook(:confirm_direct_message, :point => :pre_direct_message) do |*args|
     if config.confirm && !Client.confirm("direct #{args[0]} #{args[1]}")
       raise CommandCanceled
     end
