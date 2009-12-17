6cfd4ee02b471d1f5fb3c8467e5e1c06f78d85e8
emasaka <emasaka@gmail.com>
2009-12-17 23:20:59 +0900

    exported rl_parse_and_bind from readline.

--- lib/termtter/system_extensions.rb
+++ lib/termtter/system_extensions.rb
@@ -13,31 +13,38 @@ unless Readline.const_defined?(:NATIVE_REFRESH_LINE_METHOD)
   Readline::NATIVE_REFRESH_LINE_METHOD = Readline.respond_to?(:refresh_line)
 end
 
-unless Readline::NATIVE_REFRESH_LINE_METHOD
-  require 'dl/import'
-  module Readline
-    begin
+require 'dl/import'
+module Readline
+  begin
+    module LIBREADLINE
+      if DL.const_defined? :Importable
+        extend DL::Importable
+      else
+        extend DL::Importer
+      end
+      pathes = Array(ENV['TERMTTER_EXT_LIB'] || [
+        '/opt/local/lib/libreadline.dylib',
+        '/usr/lib/libreadline.so',
+        '/usr/local/lib/libreadline.so',
+        File.join(Gem.bindir, 'readline.dll')
+      ])
+      dlload(pathes.find { |path| File.exist?(path)})
+      extern 'int rl_parse_and_bind (char *)'
+    end
+    # Readline.readline('TAB: menu-complete')
+    def self.rl_parse_and_bind(str)
+      LIBREADLINE.rl_parse_and_bind(str.to_s)
+    end
+    unless Readline::NATIVE_REFRESH_LINE_METHOD
       module LIBREADLINE
-        if DL.const_defined? :Importable
-          extend DL::Importable
-        else
-          extend DL::Importer
-        end
-        pathes = Array(ENV['TERMTTER_EXT_LIB'] || [
-          '/opt/local/lib/libreadline.dylib',
-          '/usr/lib/libreadline.so',
-          '/usr/local/lib/libreadline.so',
-          File.join(Gem.bindir, 'readline.dll')
-        ])
-        dlload(pathes.find { |path| File.exist?(path)})
         extern 'int rl_refresh_line(int, int)'
       end
       def self.refresh_line
         LIBREADLINE.rl_refresh_line(0, 0)
       end
-    rescue Exception
-      def self.refresh_line;end
     end
+  rescue Exception
+    def self.refresh_line;end
   end
 end
 
