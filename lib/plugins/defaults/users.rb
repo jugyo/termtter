40cb63a7ecd2772159f6de10615f1b3a412b9dc2
jugyo <jugyo.org@gmail.com>
2009-12-26 02:07:23 +0900

    fixed a bug for 'profile' command.
    and delete a command 'user show'.
    Termtter::Command の修正が必要かも

--- lib/plugins/defaults/users.rb
+++ lib/plugins/defaults/users.rb
@@ -1,20 +1,20 @@
 module Termtter::Client
   register_command(
-    'user show',
-    :aliases => [:profile, :p],
+    :profile,
+    :aliases => [:p],
     :help => ["user show USERNAME", "Show user's profile."]
   ) do |arg|
-      user_name = arg.empty? ? config.user_name : arg
-      user = Termtter::API.twitter.user(user_name)
-      attrs = %w[ name screen_name url description profile_image_url location protected following
-        friends_count followers_count statuses_count favourites_count
-        id time_zone created_at utc_offset notifications
-      ]
-      label_width = attrs.map(&:size).max
-      attrs.each do |attr|
-        value = user.__send__(attr.to_sym)
-        puts "#{attr.gsub('_', ' ').rjust(label_width)}: #{value}"
-      end
+    user_name = arg.empty? ? config.user_name : arg
+    user = Termtter::API.twitter.user(user_name)
+    attrs = %w[ name screen_name url description profile_image_url location protected following
+      friends_count followers_count statuses_count favourites_count
+      id time_zone created_at utc_offset notifications
+    ]
+    label_width = attrs.map(&:size).max
+    attrs.each do |attr|
+      value = user.__send__(attr.to_sym)
+      puts "#{attr.gsub('_', ' ').rjust(label_width)}: #{value}"
+    end
   end
 
   class UserSearchEvent; attr_reader :query; def initialize(query); @query = query end; end
