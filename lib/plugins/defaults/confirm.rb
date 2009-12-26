5792d4ef89f976ac51b5dd068f4ec13d221a645a
jugyo <jugyo.org@gmail.com>
2009-12-26 12:32:00 +0900

    confirm plugin supports confirmation before delete.

--- lib/plugins/defaults/confirm.rb
+++ lib/plugins/defaults/confirm.rb
@@ -1,25 +1,30 @@
 # -*- coding: utf-8 -*-
 config.set_default(:confirm, false)
-module Termtter
-  RubytterProxy.register_hook(:confirm_update, :point => :pre_update) do |*args|
-    if config.confirm && !Client.confirm("update #{args.first}")
-      raise CommandCanceled
-    end
-    args
-  end
 
-  RubytterProxy.register_hook(:confirm_direct_message, :point => :pre_direct_message) do |*args|
-    if config.confirm && !Client.confirm("direct #{args[0]} #{args[1]}")
-      raise CommandCanceled
-    end
-    args
+def confirm(message)
+  if config.confirm && !Termtter::Client.confirm(message)
+    raise Termtter::CommandCanceled
   end
+end
 
-  RubytterProxy.register_hook(:confirm_retweet, :point => :pre_retweet) do |*args|
-    status = Termtter::API.twitter.show(args.first)
-    if config.confirm && !Client.confirm("retweet #{status.text}")
-      raise CommandCanceled
-    end
-    args
-  end
+Termtter::RubytterProxy.register_hook(:confirm_update, :point => :pre_update) do |*args|
+  confirm("update #{args.first}")
+  args
+end
+
+Termtter::RubytterProxy.register_hook(:confirm_direct_message, :point => :pre_direct_message) do |*args|
+  confirm("direct #{args[0]} #{args[1]}")
+  args
+end
+
+Termtter::RubytterProxy.register_hook(:confirm_retweet, :point => :pre_retweet) do |*args|
+  status = Termtter::API.twitter.show(args.first)
+  confirm("retweet #{status.text}")
+  args
+end
+
+Termtter::RubytterProxy.register_hook(:confirm_delete, :point => :pre_remove_status) do |*args|
+  status = Termtter::API.twitter.show(args.first)
+  confirm("delete #{status.text}")
+  args
 end
