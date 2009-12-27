a1a356bd40b6af3bc98544442ad6932f822a3086
jugyo <jugyo.org@gmail.com>
2009-12-27 21:45:26 +0900

    implemented MemoryCache

---
+++ spec/termtter/memory_cache_spec.rb
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+require File.dirname(__FILE__) + '/../spec_helper'
+
+module Termtter
+  describe MemoryCache do
+    it 'is like Hash' do
+      cache = MemoryCache.new(2)
+      cache[:foo] = 'foo'
+      cache[:bar] = 'bar'
+
+      cache[:foo].should == 'foo'
+      cache[:foo].should == 'foo'
+
+      cache[:jugyo] = 'jugyo'
+      cache[:jugyo].should == 'jugyo'
+
+      cache.key?(:foo).should be_false
+    end
+  end
+end
