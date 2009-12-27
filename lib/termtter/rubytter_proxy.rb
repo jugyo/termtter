8867890ab1b12ca3f5de90dcea6021afc1731886
jugyo <jugyo.org@gmail.com>
2009-12-27 22:04:44 +0900

    caching statuses on memory.

--- lib/termtter/rubytter_proxy.rb
+++ lib/termtter/rubytter_proxy.rb
@@ -1,3 +1,5 @@
+config.set_default(:memory_cache_size, 10000)
+
 module Termtter
   class RubytterProxy
     include Hookable
@@ -18,7 +20,7 @@ module Termtter
             modified_args = hook.call(*modified_args)
           end
 
-          result = call_rubytter(method, *modified_args, &block)
+          result = call_rubytter_or_use_cache(method, *modified_args, &block)
 
           self.class.call_hooks("post_#{method}", *args)
         rescue HookCanceled
@@ -26,6 +28,26 @@ module Termtter
         result
       else
         super
+      end
+    end
+
+    def status_cache_store
+      # TODO: DB store とかにうまいこと切り替えられるようにしたい
+      @status_cache_store ||= MemoryCache.new(config.memory_cache_size)
+    end
+
+    def call_rubytter_or_use_cache(method, *args, &block)
+      case method
+      when :show
+        status_cache_store[args[0].to_i] ||= call_rubytter(method, *args, &block)
+      when :home_timeline, :user_timeline, :friends_timeline, :search
+        statuses = call_rubytter(method, *args, &block)
+        statuses.each do |status|
+          status_cache_store[status.id] = status
+        end
+        statuses
+      else
+        call_rubytter(method, *args, &block)
       end
     end
 
