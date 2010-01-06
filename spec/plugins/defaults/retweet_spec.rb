4eae73becb23da3fd25d596d08f9b09020fec184
ujihisa <ujihisa@gmail.com>
2010-01-06 11:53:49 -0800

    More spec

--- spec/plugins/defaults/retweet_spec.rb
+++ spec/plugins/defaults/retweet_spec.rb
@@ -8,13 +8,14 @@ describe 'Termtter::Client.post_retweet' do
   end
 end
 
-# TODO: when the plugin fib is loaded'
-#it 'should add command fib' do
-#  Termtter::Client.should_receive(:register_command).once
-#  Termtter::Client.plug 'defaults/fib'
-#end
-
-#it 'should define fib method' do
-#  Termtter::Client.plug 'defaults/fib'
-#  (0..10).map {|i| fib i }.should == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-#end
+describe 'Plugin `retweet`' do
+  it 'registers a commond when it is loaded' do
+    Termtter::Client.should_receive(:register_command).at_least(5).times
+    Termtter::Client.plug 'defaults/retweet'
+  end
+#
+#  it 'should define fib method' do
+#    Termtter::Client.plug 'defaults/fib'
+#    (0..10).map {|i| fib i }.should == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+#  end
+end
