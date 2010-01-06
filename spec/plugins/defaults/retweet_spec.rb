6af74f621464554087fec36fabf145c131ee3ac6
ujihisa <ujihisa@gmail.com>
2010-01-06 13:09:14 -0800

    Shared specifications

--- spec/plugins/defaults/retweet_spec.rb
+++ spec/plugins/defaults/retweet_spec.rb
@@ -4,6 +4,44 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe 'Termtter::Client.post_retweet' do
   describe 'posts a retweet based on the given post by someone,' do
+    completely_same_as_the_following = lambda {
+      Termtter::Client.plug 'defaults/retweet'
+
+      mock = Object.new
+      def mock.user
+        mock2 = Object.new
+        def mock2.protected
+          true
+        end
+
+        def mock2.screen_name
+          'ujihisa'
+        end
+        mock2
+      end
+
+      def mock.text
+        'hi'
+      end
+
+      mock3 = Object.new
+      def mock3.update(text)
+        text.should == 'my comment RT @ujihisa: hi'
+      end
+      def mock3.retweet(id)
+        id
+      end
+
+      Termtter::API.should_receive(:twitter).and_return(mock3)
+      Termtter::Client.
+        should_receive(:confirm).
+        with('ujihisa is protected! Are you sure?', false).
+        and_return true
+      be_quiet do
+        Termtter::Client.post_retweet(mock, 'my comment')
+      end
+    }
+
     describe 'with your own comment,' do
       it 'and without confirming in the original post being not protected' do
         Termtter::Client.plug 'defaults/retweet'
@@ -37,13 +75,19 @@ describe 'Termtter::Client.post_retweet' do
       end
 
       it 'and with confirming in the original post being protected' do
+        completely_same_as_the_following.call
+      end
+    end
+
+    describe 'without your own comment,' do
+      it 'and without confirming in the original post being not protected' do
         Termtter::Client.plug 'defaults/retweet'
 
         mock = Object.new
         def mock.user
           mock2 = Object.new
           def mock2.protected
-            true
+            false
           end
 
           def mock2.screen_name
@@ -56,22 +100,23 @@ describe 'Termtter::Client.post_retweet' do
           'hi'
         end
 
-        mock3 = Object.new
-        def mock3.update(text)
-          text.should == 'my comment RT @ujihisa: hi'
+        def mock.id
+          123
         end
+
+        mock3 = Object.new
         def mock3.retweet(id)
-          id
+          id.should == 123
         end
 
         Termtter::API.should_receive(:twitter).and_return(mock3)
-        Termtter::Client.
-          should_receive(:confirm).
-          with('ujihisa is protected! Are you sure?', false).
-          and_return true
         be_quiet do
-          Termtter::Client.post_retweet(mock, 'my comment')
+          Termtter::Client.post_retweet(mock)
         end
+      end
+
+      it 'and with confirming in the original post being protected' do
+        completely_same_as_the_following.call
       end
     end
   end
