cdb1f3f6cba8f1ec9d7611c835a702590c6b2cc9
ujihisa <ujihisa@gmail.com>
2010-01-06 12:20:42 -0800

    Added spec of Termtter::Client.post_retweet posts a retweet based on the given post by someone, with your own comment, and without confirming in the original post being not protected

--- spec/plugins/defaults/retweet_spec.rb
+++ spec/plugins/defaults/retweet_spec.rb
@@ -3,8 +3,39 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe 'Termtter::Client.post_retweet' do
-  it 'posts a retweet based on the given post by someone, confirming if it is protected' do
-    pending
+  describe 'posts a retweet based on the given post by someone,' do
+    describe 'with your own comment,' do
+      it 'and without confirming in the original post being not protected' do
+        Termtter::Client.plug 'defaults/retweet'
+
+        mock = Object.new
+        def mock.user
+          mock2 = Object.new
+          def mock2.protected
+            false
+          end
+
+          def mock2.screen_name
+            'ujihisa'
+          end
+          mock2
+        end
+
+        def mock.text
+          'hi'
+        end
+
+        mock3 = Object.new
+        def mock3.update(text)
+          text
+        end
+
+        Termtter::API.should_receive(:twitter).and_return(mock3)
+        $stdout = StringIO.new
+        Termtter::Client.post_retweet(mock, 'my comment')
+        $stdout = STDOUT
+      end
+    end
   end
 end
 
@@ -13,9 +44,4 @@ describe 'Plugin `retweet`' do
     Termtter::Client.should_receive(:register_command).at_least(5).times
     Termtter::Client.plug 'defaults/retweet'
   end
-#
-#  it 'should define fib method' do
-#    Termtter::Client.plug 'defaults/fib'
-#    (0..10).map {|i| fib i }.should == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-#  end
 end
