ec28ffc36fbda5d298b0f48f2794e286870c0ef8
ujihisa <ujihisa@gmail.com>
2010-01-06 12:58:44 -0800

    Added spec for proctected user's RT

--- spec/plugins/defaults/retweet_spec.rb
+++ spec/plugins/defaults/retweet_spec.rb
@@ -35,6 +35,44 @@ describe 'Termtter::Client.post_retweet' do
           Termtter::Client.post_retweet(mock, 'my comment')
         end
       end
+
+      it 'and with confirming in the original post being protected' do
+        Termtter::Client.plug 'defaults/retweet'
+
+        mock = Object.new
+        def mock.user
+          mock2 = Object.new
+          def mock2.protected
+            true
+          end
+
+          def mock2.screen_name
+            'ujihisa'
+          end
+          mock2
+        end
+
+        def mock.text
+          'hi'
+        end
+
+        mock3 = Object.new
+        def mock3.update(text)
+          text.should == 'my comment RT @ujihisa: hi'
+        end
+        def mock3.retweet(id)
+          id
+        end
+
+        Termtter::API.should_receive(:twitter).and_return(mock3)
+        Termtter::Client.
+          should_receive(:confirm).
+          with('ujihisa is protected! Are you sure?', false).
+          and_return true
+        be_quiet do
+          Termtter::Client.post_retweet(mock, 'my comment')
+        end
+      end
     end
   end
 end
