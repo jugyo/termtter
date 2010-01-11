1a3bd89f0e30e24a7b665413038f2a7748dbd968
jugyo <jugyo.org@gmail.com>
2010-01-11 15:08:41 +0900

    fix specs for list

--- spec/plugins/defaults/list_spec.rb
+++ spec/plugins/defaults/list_spec.rb
@@ -2,33 +2,32 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe 'plugin lists' do
   before do
+    Termtter::Client.setup_task_manager
     config.user_name = 'jugyo'
     @twitter_stub = Object.new
     Termtter::API.stub!(:twitter).and_return(@twitter_stub)
   end
 
-  describe 'command lists' do
+  describe 'command list' do
     before do
       Termtter::Client.plug 'defaults'
-      @command = Termtter::Client.commands[:lists]
+      @command = Termtter::Client.commands[:list]
     end
 
     it 'command name is :lists' do
-      @command.name.should == :lists
+      @command.name.should == :list
     end
 
     it 'should call with no user_name' do
-      response = Object.new
-      response.stub!(:lists).and_return({})
-      @twitter_stub.should_receive(:lists).with('jugyo').and_return(response)
-      Termtter::Client.execute('lists')
+      response = []
+      @twitter_stub.should_receive(:home_timeline).and_return(response)
+      Termtter::Client.execute('list')
     end
 
     it 'should call with user_name' do
-      response = Object.new
-      response.stub!(:lists).and_return({})
-      @twitter_stub.should_receive(:lists).with('termtter').and_return(response)
-      Termtter::Client.execute('lists termtter')
+      response = []
+      @twitter_stub.should_receive(:user_timeline).with('termtter', {}).and_return(response)
+      Termtter::Client.execute('list termtter')
     end
   end
 end
