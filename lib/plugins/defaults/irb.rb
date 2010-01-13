0d88a647609c959aa051dbdc64c50700eb0d7f78
jugyo <jugyo.org@gmail.com>
2010-01-14 00:44:18 +0900

    fix to initialize IRB

--- lib/plugins/defaults/irb.rb
+++ lib/plugins/defaults/irb.rb
@@ -1,4 +1,25 @@
+require 'irb'
+
+module IRB
+  def self.start_session(binding)
+    unless @__irb_initialized
+      IRB.setup(nil)
+      @__irb_initialized = true
+    end
+
+    workspace = WorkSpace.new(binding)
+
+    irb = Irb.new(workspace)
+
+    @CONF[:IRB_RC].call(irb.context) if @CONF[:IRB_RC]
+    @CONF[:MAIN_CONTEXT] = irb.context
+
+    catch(:IRB_EXIT) do
+      irb.eval_input
+    end
+  end
+end
+
 Termtter::Client.register_command(:irb) do |args|
-  require 'irb'
-  IRB.start
+  IRB.start_session(binding)
 end
