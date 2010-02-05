3245590cb12a0d6483284f5846e7b635bdd658a3
ujihisa <ujihisa@gmail.com>
2010-02-04 18:08:07 -0800

    Refactoring: shorter each lines

--- lib/plugins/notify-send2.rb
+++ lib/plugins/notify-send2.rb
@@ -1,33 +1,41 @@
 # -*- coding: utf-8 -*-
-
 require 'fileutils'
+config.plugins.notify_send.set_default(
+  :icon_cache_dir, "#{Termtter::CONF_DIR}/tmp/user_profile_images")
 
-config.plugins.notify_send.set_default(:icon_cache_dir, "#{Termtter::CONF_DIR}/tmp/user_profile_images")
-
-FileUtils.mkdir_p(config.plugins.notify_send.icon_cache_dir) unless File.exist?(config.plugins.notify_send.icon_cache_dir)
-Dir.glob("#{config.plugins.notify_send.icon_cache_dir}/*") {|f| File.delete(f) unless File.size?(f) }
+FileUtils.mkdir_p(config.plugins.notify_send.icon_cache_dir) unless
+  File.exist?(config.plugins.notify_send.icon_cache_dir)
+Dir.glob("#{config.plugins.notify_send.icon_cache_dir}/*") {|f|
+  File.delete(f) unless File.size?(f) }
 unless File.exist?("#{config.plugins.notify_send.icon_cache_dir}/default.png")
-  File.open("#{config.plugins.notify_send.icon_cache_dir}/default.png", "wb") do |f|
-    f << open("http://static.twitter.com/images/default_profile_normal.png").read
+  File.open(
+    "#{config.plugins.notify_send.icon_cache_dir}/default.png", "wb") do |f|
+    f << open(
+      "http://static.twitter.com/images/default_profile_normal.png").read
   end
 end
 
 def get_icon_path(s)
   /https?:\/\/.+\/(\d+)\/.*?$/ =~ s.user.profile_image_url
-  cache_file = "%s/%s-%s%s" % [  config.plugins.notify_send.icon_cache_dir,
-                                 s.user.screen_name,
-                                 $+,
-                                 File.extname(s.user.profile_image_url)  ]
+  cache_file = "%s/%s-%s%s" % [
+    config.plugins.notify_send.icon_cache_dir,
+    s.user.screen_name,
+    $+,
+    File.extname(s.user.profile_image_url)]
   unless File.exist?(cache_file)
     Thread.new(s,cache_file) do |s,cache_file|
-      Dir.glob("#{config.plugins.notify_send.icon_cache_dir}/#{s.user.screen_name}-*") {|f| File.delete(f) }
+      Dir.glob(
+        "#{config.plugins.notify_send.icon_cache_dir}/#{s.user.screen_name}-*") {|f|
+        File.delete(f)
+      }
       begin
         s.user.profile_image_url.sub!(/^https/,'http')
         File.open(cache_file, 'wb') do |f|
           f << open(URI.escape(s.user.profile_image_url)).read
         end
       rescue OpenURI::HTTPError
-        cache_file = "#{config.plugins.notify_send.icon_cache_dir}/default.png"
+        cache_file =
+          "#{config.plugins.notify_send.icon_cache_dir}/default.png"
       end
     end
   end
@@ -42,11 +50,13 @@ Termtter::Client.register_hook(
     Thread.start do
       statuses.each do |s|
         text = CGI.escapeHTML(s.text)
-        text.gsub!(%r{https?://[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+},'<a href="\0">\0</a>')
-        system 'notify-send', '-i', get_icon_path(s), '--', s.user.screen_name, text
+        text.gsub!(
+          %r{https?://[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+},
+          '<a href="\0">\0</a>')
+        system(
+          'notify-send', '-i', get_icon_path(s), '--', s.user.screen_name, text)
         sleep 0.1
       end
     end
   }
 )
-
