1a851128afde5e61e865f07ef7fc98ed4881b2bb
ujihisa <ujihisa@gmail.com>
2010-02-11 11:46:33 -0800

    bugfix

--- lib/plugins/clock.rb
+++ lib/plugins/clock.rb
@@ -1,3 +1,10 @@
 module Termtter::Client
-  config.prompt = Time.now.strftime('%I:%M%p').downcase << ' > '
+  register_hook(
+    :name => :clock_prompt,
+    :point => :prepare_prompt,
+    :exec => lambda {|prompt|
+      time = Time.now.strftime('%I:%M%p').downcase
+      config.prompt = "#{time} #{public_storage[:orig_prompt]}"
+    }
+  )
 end
