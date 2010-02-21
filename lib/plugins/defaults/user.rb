104a4bf992c738d3887b856c177ad0de4c66e6f9
ujihisa <ujihisa@gmail.com>
2010-02-21 14:28:01 -0800

    Natural English

--- lib/plugins/defaults/user.rb
+++ lib/plugins/defaults/user.rb
@@ -26,7 +26,7 @@ module Termtter::Client
     search_option = config.user_search.option.empty? ? {} : config.user_search.option
     users = Termtter::API.twitter.search_user(arg, search_option)
     users.each do |user|
-      puts "@#{user.screen_name} (#{user.name}) : #{user.description}"
+      puts "@#{user.screen_name} (#{user.name}): #{user.description}"
     end
   end
 end
