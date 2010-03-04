2fec49a7e9541b257032920d697f73a75d96c969
sorah <sora134@gmail.com>
2010-03-04 14:29:16 +0900

    Fix ruby-growl for multibyte chars if Ruby version is over 1.9

--- lib/plugins/growl.rb
+++ lib/plugins/growl.rb
@@ -8,6 +8,53 @@ require 'cgi'
 
 begin
   require 'ruby-growl'
+  if RUBY_VERSION >= "1.9" # Fix ruby-growl for multibyte chars if Ruby version is over 1.9
+    class Growl
+      private
+
+      def notification_packet(name, title, description, priority, sticky)
+        flags = 0
+        data = []
+
+        packet = [
+          GROWL_PROTOCOL_VERSION,
+          GROWL_TYPE_NOTIFICATION,
+        ]
+
+        flags = 0
+        flags |= ((0x7 & priority) << 1) # 3 bits for priority
+        flags |= 1 if sticky # 1 bit for sticky
+
+        packet << flags
+        packet << name.bytesize
+        packet << title.bytesize
+        packet << description.bytesize
+        packet << @app_name.bytesize
+
+        data << name
+        data << title
+        data << description
+        data << @app_name
+
+        packet << data.join
+        packet = packet.pack GNN_FORMAT
+
+        checksum = MD5.new packet
+        checksum.update @password unless @password.nil?
+
+        packet << checksum.digest
+
+        return packet.force_encoding('utf-8')
+      end
+
+      def send(packet)
+        set_sndbuf packet.bytesize
+        @socket.send packet, 0
+        @socket.flush
+      end
+    end
+  end
+
   growl = Growl.new "localhost", "termtter", ["update_friends_timeline"]
 rescue LoadError
   growl = nil
