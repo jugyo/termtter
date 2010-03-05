63f63c9c365ef2da2926f217fc414db5f89c1187
sorah <sora134@gmail.com>
2010-03-05 16:14:42 +0900

    Fix reply_sound.rb

--- lib/plugins/reply_sound.rb
+++ lib/plugins/reply_sound.rb
@@ -7,15 +7,18 @@ config.plugins.reply_sound.set_default(:interval, 600)
 config.plugins.reply_sound.set_default(:sound_file, '/System/Library/Sounds/Hero.aiff')
 
 reply_sound_cache = nil
+reply_sound_cache_ids = []
 
 Termtter::Client.add_task(:name => :reply_sound,
                           :interval => config.plugins.reply_sound.interval) do
   replies = Termtter::API.twitter.replies
-  if !reply_sound_cache.nil? && (replies - reply_sound_cache).size > 0
+  new_replies = replies.delete_if{|x| reply_sound_cache_ids.index(x[:id]) }
+  if !reply_sound_cache.nil? && new_replies.size > 0
     system 'afplay "'+config.plugins.reply_sound.sound_file+'" 2>/dev/null &'
-    Termtter::Client.output(replies - reply_sound_cache,:new_replies,:replies)
+    Termtter::Client.output(new_replies,:new_replies,:replies)
   end
   reply_sound_cache = replies
+  reply_sound_cache_ids += replies.map{|x| x[:id]}
 end
 
 
