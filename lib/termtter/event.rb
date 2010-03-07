9f6a2632886fd740926e3016d62c423147c9f19d
hitode909 <hitode909@gmail.com>
2010-03-07 17:21:59 +0900

    event class

---
+++ lib/termtter/event.rb
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+require 'termtter/active_rubytter'
+
+module Termtter
+  class Event
+    def initialize(name, params = {})
+      raise TypeError unless name.kind_of? Symbol
+      raise TypeError unless params.kind_of? Hash
+      @name = name
+      @params = ActiveRubytter.new(params)
+    end
+
+    attr_reader :name
+    undef_method :to_s
+
+    def method_missing(name, *args)
+      @name.__send__(name, *args)
+    rescue NoMethodError
+      @params.__send__(name, *args)
+    end
+
+    def hash
+      @name.hash
+    end
+
+    def has_key?(key)
+      @params.to_hash.has_key?(key)
+    end
+
+    def set_param(key, value)
+      data = @params.to_hash
+      data[key] = value
+      @params.attributes = data
+      value
+    end
+
+    alias_method :[]=, :set_param
+
+    def  ==(b)
+      if b.kind_of? Event
+        @name == b.name
+      else
+        @name == b
+      end
+    end
+  end
+end
