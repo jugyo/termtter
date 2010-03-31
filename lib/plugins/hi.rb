78463c20241ffe0dd2e84fd5ead18672a1a0f10b
ujihisa <ujihisa@gmail.com>
2010-03-31 16:35:43 -0700

    Added hey, hello, how_are_you, how_s_going and como_estas_guten_tag

--- lib/plugins/hi.rb
+++ lib/plugins/hi.rb
@@ -1,15 +1,24 @@
 # -*- coding: utf-8 -*-
+# あいうえお
 module Termtter::Client
-  [:hi, :hola].each do |hi|
-    register_command(hi, :help => ["#{hi} [(Optinal) USER]", "Post a #{hi}"]) do |arg|
-      result =
-        if arg.empty?
-          Termtter::API.twitter.update(hi.to_s)
-        else
-          name = normalize_as_user_name(arg)
-          Termtter::API.twitter.update("@#{name} #{hi}")
-        end
-      puts "=> " << result.text
+  {
+    :english => ['hi', 'hey', 'hello', 'How are you?', "How's going?"],
+    :spanish => ['hola', '¡Hola!', '¿Cómo estás?'],
+    :german => ['Guten Tag!'],
+  }.each do |language, greetings|
+    greetings.each do |greeting|
+      # '¿Cómo estás?' -> 'como_estas'
+      command_name = greeting.tr('áó', 'ao').scan(/\w+/).join('_').downcase
+      register_command(command_name, :help => ["#{command_name} [(Optinal) USER]", "Post a greeting message in #{language.to_s.capitalize}"]) do |arg|
+        result =
+          if arg.empty?
+            Termtter::API.twitter.update(greeting)
+          else
+            name = normalize_as_user_name(arg)
+            Termtter::API.twitter.update("@#{name} #{greeting}")
+          end
+        puts "=> " << result.text
+      end
     end
   end
 end
