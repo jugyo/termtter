95b0b4bdc4f05d67c782bd4f6b71836721554248
ujihisa <ujihisa@gmail.com>
2010-03-31 16:43:42 -0700

    Specbugfix

--- spec/plugins/hi_spec.rb
+++ spec/plugins/hi_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe Termtter::Client, 'when the plugin fib is loaded' do
-  it 'adds command hi and hola' do
+describe Termtter::Client, 'when the plugin hi is loaded' do
+  it 'adds commands such as hi and hola' do
     Termtter::Client.should_receive(:register_command).at_most(9).times
     Termtter::Client.plug 'hi'
   end
