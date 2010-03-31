8fa26b96972cd1cc1c3ad36c0e545e03439f13d5
ujihisa <ujihisa@gmail.com>
2010-03-31 16:51:58 -0700

    Added capital_update(cu) command

---
+++ spec/plugins/capital_update_spec.rb
@@ -0,0 +1,9 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Termtter::Client, 'when the plugin capital_update is loaded' do
+  it 'adds command capital_update' do
+    Termtter::Client.should_receive(:register_command).once
+    Termtter::Client.plug 'capital_update'
+  end
+end
+
