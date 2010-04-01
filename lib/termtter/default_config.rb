5fb71cbdc4f6fcf2fe6ce26371e3dcbd8ae4928e
Sora Harakami <sora134@gmail.com>
2010-04-01 09:29:38 +0900

    April-fool (Fix lib/termtter/default_config.rb if april-fool end)

--- lib/termtter/default_config.rb
+++ lib/termtter/default_config.rb
@@ -6,7 +6,7 @@ config.set_default(:timeout, 5)
 config.set_default(:retry, 1)
 config.set_default(:splash, <<SPLASH)
 
-   <cyan>&lt;(@)//_</cyan>  .  .      <on_green> Termtter <underline>#{Termtter::VERSION}</underline> </on_green>
+   <cyan>&lt;(@)//_</cyan>  .  .      <on_green> #{(Time.now.year == 2010 && Time.now.month == 4 && Time.now.day == 1)?'Centertter':'Termtter'} <underline>#{Termtter::VERSION}</underline> </on_green>
       <cyan>\\\\</cyan>           <on_green> http://termtter.org/ </on_green>
 
 SPLASH
