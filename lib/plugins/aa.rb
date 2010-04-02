c2ecdb4e5166a1ad0702c4a4e1aab159c7a930a6
hitode909 <hitode909@gmail.com>
2010-04-02 09:59:24 +0900

    aa command

---
+++ lib/plugins/aa.rb
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+module AAMaker
+  def self.random(n)
+    rand(n).to_i
+  end
+  def self.randSelect(array)
+    return array[random(array.length)]
+  end
+
+  def self.make
+    rinkaku = randSelect([['(', ')'], ['(', ')'], ['|', '|'], ['｜', '｜']]);
+    otete = randSelect([['', '', '', '', ''], ['', '', 'm', '', ''], ['', '', 'ლ', '', ''], ['ლ', '', '', 'ლ', ''], ['', '｢', '', '', '｢'], ['', ' つ', '', '', 'つ'], ['', ' ', '', '', 'o彡ﾟ'], ['', 'n', '', '', 'η'], ['', '∩', '', '∩', ''], ['∩', '', '', '', '∩'], ['ヽ', '', '', '', 'ノ'], ['┐', '', '', '', '┌'], ['╮', '', '', '', '╭'], ['<', '', '', '', '/'], ['╰', '', '', ' ', ''], ['o', '', '', '', 'o'], ['o', '', '', '', 'ツ'], ['', '', '', '', 'ﾉｼ']]);
+    omeme = randSelect([['\u25d5', '\u25d5'], ['╹', '╹'], ['＞', '＜'], ['＾', '＾'], ['・', '・'], ['´・', '・`'], ['`・', '・´'], ['´', '`'], ['≧', '≦'], ['ﾟ', 'ﾟ'], ['\'', '\''], ['･ิ', '･ิ'], ['❛', '❛'], ['⊙', '⊙'], ['￣', '￣'], ['◕ˇ', 'ˇ◕']]);
+    okuti = randSelect(['ω', '∀', '▽', '△', 'Д' , '□', '～', 'ー', 'ェ', 'ρ', 'o']);
+    hoppe = randSelect([['', ''], ['*', ''], ['', '*'], ['', '#'], ['#', ''], ['✿', ''], ['', '✿'], ['', '；'], ['；', ''], ['｡', '｡'], ['｡', ''], ['', '｡'], ['▰', '▰'], ['', '▰'], ['▰', ''], ['๑', '๑'], ['', '๑'], ['๑', '']]);
+
+    text = [
+      otete[0],
+      rinkaku[0],
+      otete[1] || (otete[3] ? '' : hoppe[0]),
+      omeme[0],
+      otete[2] || okuti,
+      omeme[1],
+      otete[3] || (otete[1] ? '' : hoppe[1]),
+      rinkaku[1],
+      otete[4]
+    ].join('')
+
+    text
+  end
+end
+
+Termtter::Client.register_command(
+  :name => :aa,
+  :exec_proc => lambda {|arg|
+    name = Termtter::Client.normalize_as_user_name(arg)
+    command = name.length > 0 ? "u @#{name} #{AAMaker.make}" : "u #{AAMaker.make}"
+    Termtter::Client.execute command
+  }
+)
