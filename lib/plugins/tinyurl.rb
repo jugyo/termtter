77eb3d0d6ef7cb622543ef86ce0756750d5f5596
emasaka <emasaka@gmail.com>
2010-04-06 08:58:27 +0900

    Separated shorten_url method.

--- lib/plugins/tinyurl.rb
+++ lib/plugins/tinyurl.rb
@@ -9,34 +9,44 @@ config.plugins.tinyurl.set_default(:shorturl_makers, [
 config.plugins.tinyurl.set_default(:tinyurl_hook_commands, [:update, :reply, :retweet])
 config.plugins.tinyurl.set_default(:uri_regexp, URI.regexp(%w(http https ftp)))
 
-Termtter::Client.register_hook(
-  :name => :tinyurl,
-  :points => config.plugins.tinyurl.tinyurl_hook_commands.map {|cmd|
-    "modify_arg_for_#{cmd.to_s}".to_sym
-  },
-  :exec_proc => lambda {|cmd, arg|
-    arg.gsub(/#{config.plugins.tinyurl.uri_regexp}\S*/) do |url|
-      url_enc = URI.escape(url, /[^a-zA-Z0-9.:]/)
-      result = url
-      config.plugins.tinyurl.shorturl_makers.each do |site|
-        res = Termtter::HTTPpool.start(site[:host]) do |h|
-          h.get(site[:format] % url_enc)
+module Termtter::Client
+  register_hook(
+    :name => :tinyurl,
+    :points => config.plugins.tinyurl.tinyurl_hook_commands.map {|cmd|
+      "modify_arg_for_#{cmd.to_s}".to_sym
+    },
+    :exec_proc => lambda {|cmd, arg|
+      arg.gsub(/#{config.plugins.tinyurl.uri_regexp}\S*/) do |url|
+        result = nil
+        config.plugins.tinyurl.shorturl_makers.each do |site|
+          result = shorten_url(url, site[:host], site[:format])
+          break if result
         end
-        if res.code == '200'
-          result = res.body
-          if /"shortUrl": "(http.*)"/ =~ result
-            result = $1
-          elsif /"statusCode": "ERROR"/ =~ result
-            result = url
-            next
-          end
-          break
-        end
+        result or url
+      end
+    }
+  )
+
+  # returns nil if not shorten
+  def self.shorten_url(url, host, format)
+    url_enc = URI.escape(url, /[^a-zA-Z0-9.:]/)
+    res = Termtter::HTTPpool.start(host) do |h|
+      h.get(format % url_enc)
+    end
+    if res.code == '200'
+      result = res.body
+      if /"shortUrl": "(http.*)"/ =~ result
+        result = $1
+      elsif /"statusCode": "ERROR"/ =~ result
+        return nil
       end
       result
+    else
+      nil
     end
-  }
-)
+  end
+
+end
 
 # tinyuri.rb
 # make URLs in your update to convert tinyurl.com/XXXXXXX.
