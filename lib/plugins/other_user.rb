e47cf3a6036ed0721a678c8f01fe7d67b11e8958
Sora Harakami <sora134@gmail.com>
2010-04-26 13:37:25 +0900

    other_user plug

---
+++ lib/plugins/other_user.rb
@@ -0,0 +1,16 @@
+config.plugins.other_user.set_default(:accounts,{})
+config.plugins.other_user.set_default(:alias,{})
+
+Termtter::Client.register_command(
+  :name => :other_user,
+  :alias => :o,
+  :help => ['other_user, o','Post by other user'],
+  :exec => lambda do |arg_raw|
+    body = arg_raw.split(/ /)
+    user_raw = body.shift
+    user = config.plugins.other_user.alias[user_raw] || user_raw
+    Rubytter.new(user,config.plugins.other_user.accounts[user]).update(body.join(''))
+    puts "updated by #{user} => #{body.join('')}"
+  end
+)
+
