bc7288dbbd031bf801daf8023722a6ebae433601
Sora Harakami <sora134@gmail.com>
2010-04-27 18:51:15 +0900

    crypt_spec

---
+++ spec/termtter/crypt_spec.rb
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+require File.dirname(__FILE__) + '/../spec_helper'
+require 'termtter/crypt'
+
+module Termtter
+  describe Crypt do
+    it '.crypt crypts string' do
+      Crypt.crypt('hi').should be_kind_of(String)
+    end
+    it '.decrypt decrypts string' do
+      a = Crypt.crypt('hi')
+      Crypt.decrypt(a).should == 'hi'
+    end
+  end
+end
