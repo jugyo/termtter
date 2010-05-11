f1bd516beaa29ec16608f9e2b8122509ef8411b5
Sora Harakami <sora134@gmail.com>
2010-05-12 07:01:08 +0900

    other_user oauth

--- lib/plugins/other_user.rb
+++ lib/plugins/other_user.rb
@@ -1,4 +1,7 @@
+require 'base64'
+
 config.plugins.other_user.set_default(:accounts,{})
+config.plugins.other_user.set_default(:tokens_file,"~/.termtter/other_user_tokens")
 config.plugins.other_user.set_default(:alias,{})
 
 Termtter::Client.register_command(
@@ -6,10 +9,38 @@ Termtter::Client.register_command(
   :alias => :o,
   :help => ['other_user, o','Post by other user'],
   :exec => lambda do |arg_raw|
+    tokens = if File.exist?(File.expand_path(config.plugins.other_user.tokens_file))
+               Marshal.load(
+                 Base64.decode64(File.read(File.expand_path(
+                   config.plugins.other_user.tokens_file))))
+             else
+               {}
+             end
+
     body = arg_raw.split(/ /)
     user_raw = body.shift
     user = config.plugins.other_user.alias[user_raw] || user_raw
-    Rubytter.new(user,config.plugins.other_user.accounts[user]).update(body.join(' '))
+
+    unless tokens[user]
+      puts "<on_red>ERROR</on_red> #{user} isn't authorized yet. Starting authorization...".termcolor
+      tokens[user] = Termtter::API.authorize_by_oauth(false,false,false)
+      open(File.expand_path(config.plugins.other_user.tokens_file), 'w') do |f|
+        f.print Base64.encode64(Marshal.dump(tokens))
+      end
+    end
+
+    at = OAuth::AccessToken.new(OAuth::Consumer.new(
+                                  Termtter::Crypt.decrypt(Termtter::CONSUMER_KEY),
+                                  Termtter::Crypt.decrypt(Termtter::CONSUMER_SECRET),
+                                  :site => "http://twitter.com/",
+                                  :proxy => ENV['http_proxy']
+                                ),
+                                tokens[user][:token],
+                                tokens[user][:secret])
+
+    t = OAuthRubytter.new(at, Termtter::API.twitter_option)
+    t.update(body.join(' '))
+    
     puts "updated by #{user} => #{body.join(' ')}"
   end
 )
