b65253969863e8188c5d8b5541435ef9d26f85fa
jugyo <kkohno@gmail.com>
2010-05-12 19:14:51 +0900

    time_signal plugin

---
+++ lib/plugins/time_signal.rb
@@ -0,0 +1,21 @@
+config.plugins.time_signal.set_default(:minutes, [0])
+
+last_signal_time = nil
+
+Termtter::Client.add_task(:name => :time_signal, :interval => 10) do
+  begin
+    now = Time.now
+    if config.plugins.time_signal.minutes.include?(now.min)
+      hour = now.strftime('%H:%M')
+      unless hour == last_signal_time
+        print "\e[0G\e[2K"
+        puts "<on_green> #{hour} </on_green>".termcolor
+        Termtter::Client.notify 'time signal', hour
+        Readline.refresh_line
+        last_signal_time = hour
+      end
+    end
+  rescue Exception => e
+    Termtter::Client.handle_error(e)
+  end
+end
