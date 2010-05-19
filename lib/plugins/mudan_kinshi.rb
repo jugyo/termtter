a0811d14063628be36b885c995efe9bea256f76f
ujihisa <ujihisa@gmail.com>
2010-05-19 18:31:00 -0400

    Refactoring

--- lib/plugins/mudan_kinshi.rb
+++ lib/plugins/mudan_kinshi.rb
@@ -2,7 +2,8 @@
 # http://twitter.com/ototorosama/status/14283311303
 
 Termtter::Client.register_hook(
-  :name => :mudan_kinsi,
+  :name => :mudan_kinshi,
+  :author => 'Sora Harakami',
   :point => :pre_coloring,
   :exec => lambda {|r,e|
     r.gsub(/無断(.+?)禁止/) {|s|
