55197c214abeb383670eaac80fb315162dde34f0
ujihisa <ujihisa@gmail.com>
2010-05-19 18:57:00 -0400

    Newly introduced mecab plubin

---
+++ lib/plugins/mecab.rb
@@ -0,0 +1,23 @@
+# coding: utf-8
+$KCODE = 'u' unless defined?(Encoding)
+# mecab('これはテストです') #=>
+#   [["これ", "名詞", "代名詞", "一般", "*", "*", "*", "これ", "コレ", "コレ"],
+#    ["は", "助詞", "係助詞", "*", "*", "*", "*", "は", "ハ", "ワ"],
+#    ["テスト", "名詞", "サ変接続", "*", "*", "*", "*", "テスト", "テスト", "テスト"],
+#    ["です", "助動詞", "*", "*", "*", "特殊・デス", "基本形", "です", "デス", "デス"]]
+def mecab(str)
+  IO.popen('mecab', 'r+') {|io|
+    io.puts str
+    io.close_write
+    io.read.split(/\n/).map {|i| i.split(/\t|,/) }[0..-2]
+  }
+end
+
+Termtter::Client.register_command(
+  :name => 'mecab',
+  :help => 'post a Japanese message with syntaxtic explanations',
+  :exec => lambda {|arg|
+    text = mecab(arg).map {|i| "#{i[0]}(#{i[1]}: #{i[2]})" }.join
+    update(text)
+    puts "=> #{test}"
+  })
