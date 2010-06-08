36735e36d7dcd864b5150e7c51a1f0a6d3deca0d
Sora Harakami <sora134@gmail.com>
2010-06-08 09:13:32 +0900

    Joke

--- lib/plugins/defaults/fib.rb
+++ lib/plugins/defaults/fib.rb
@@ -1,7 +1,16 @@
 def fib(n)i=0;j=1;n.times{j=i+i=j};i end
-Termtter::Client.register_command(:fib) do |arg|
-  n = arg.to_i
-  text = "fib(#{n}) = #{fib n}"
-  Termtter::API.twitter.update(text)
-  puts "=> " << text
-end
+Termtter::Client.register_command(:name => :fib,
+                                  :aliases => [:f, :ho],
+                                  :exec => lambda do |arg|
+  case arg
+  when "ukumori"
+    puts 'Does it mean "Sora Harakami (@sora_h)"?'
+  when "ootsuite", "otsuite"
+    puts "NDA :D"
+  else
+    n = arg.to_i
+    text = "fib(#{n}) = #{fib n}"
+    Termtter::API.twitter.update(text)
+    puts "=> " << text
+  end
+end)
