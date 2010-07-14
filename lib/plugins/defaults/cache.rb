0d745aa2dd935017c6ce0bae6c00f52bb69244e1
MATSUYAMA Kengo <macksx@gmail.com>
2010-07-14 22:23:49 +0900

    Fix help messages for "cache (stats|flush)".

--- lib/plugins/defaults/cache.rb
+++ lib/plugins/defaults/cache.rb
@@ -3,14 +3,14 @@ require 'pp'
 module Termtter::Client
   register_command(
     :name => :"cache stats",
-    :help => 'Show Memcached stats.',
+    :help => ['cache stats', 'Show Memcached stats.'],
     :exec_proc => lambda {|arg|
       puts memory_cache.stats.pretty_inspect
     })
 
   register_command(
     :name => :"cache flush",
-    :help => 'Flush all caches.',
+    :help => ['cache flush', 'Flush all caches.'],
     :exec_proc => lambda {|arg|
       memory_cache.flush_all
       logger.info "cache flushed."
