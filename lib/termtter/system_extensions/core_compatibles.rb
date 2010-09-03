08aa0dba5b2040a9df39ee8727c2696bec3169b0
emasaka <emasaka@gmail.com>
2010-09-03 21:37:35 +0900

    instance_methods.include? -> respod_to?
    In Ruby 1.8, Object#instance_methods returns Array of String.
    In Ruby 1.9, Object#instance_methods returns Array of Symbol.

--- lib/termtter/system_extensions/core_compatibles.rb
+++ lib/termtter/system_extensions/core_compatibles.rb
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-unless Array.instance_methods.include?(:take)
+unless [].respond_to?(:take)
   class Array
     def take(n) self[0...n] end
   end
 end
 
-unless Symbol.instance_methods.include?(:to_proc)
+unless :to_proc.respond_to?(:to_proc)
   class Symbol
     def to_proc
       Proc.new { |*args| args.shift.__send__(self, *args) }
@@ -14,7 +14,7 @@ unless Symbol.instance_methods.include?(:to_proc)
   end
 end
 
-unless Symbol.instance_methods.include?(:size)
+unless :size.respond_to?(:size)
   class Symbol
     def size
       self.to_s.size
