bd2e026c3c2e3b55d3da5c831a07e38c43cc4aa7
hitode909 <hitode909@gmail.com>
2011-01-24 19:32:35 +0900

    commands using MongoD

--- lib/plugins/mongo.rb
+++ lib/plugins/mongo.rb
@@ -2,11 +2,17 @@ require 'mongo'
 config.plugins.mongo.set_default(:db_name, 'twitter')
 
 module Termtter::Client
-  db = Mongo::Connection.new.db(config.plugins.mongo.db_name)
+  class << self
+    def mongo_db
+      @mongo_db ||= Mongo::Connection.new.db(config.plugins.mongo.db_name)
+    end
+  end
+end
 
+module Termtter::Client
   register_hook(
     :name => :user_stream_insert_mongo,
-    :point => :user_stream_receive,
+    :poipnt => :user_stream_receive,
     :exec => lambda {|chunk|
       data = JSON.parse(chunk)
 
@@ -15,9 +21,29 @@ module Termtter::Client
       : data['delete'] ? 'delete'
       : 'status'
 
-      db.collection(coll_key).insert(data)
+      mongo_db.collection(coll_key).insert(data)
     })
 
+  register_command(
+    :name => :mongo_favs,
+    :exec_proc => lambda {|arg|
+      mongo_db.collection('event').find({event: "favorite"}).sort(:$natural, -1).limit(50).to_a.reverse.each{|event|
+        puts "#{event['source']['screen_name']} #{event['event']} #{event['target']['screen_name']}: #{event['target_object']['text']}"
+      }
+    },
+    :help => ["mongo_favs", "Print favorites from MongoDB"]
+    )
+
+  register_command(
+    :name => :mongo_favs,
+    :exec_proc => lambda {|arg|
+      mongo_db.collection('event').find({event: "follow"}).sort(:$natural, -1).limit(50).to_a.reverse.each{|event|
+        puts "#{event['source']['screen_name']} #{event['event']} #{event['target']['screen_name']}"
+      }
+    },
+    :help => ["mongo_follows", "Print follows from MongoDB"]
+    )
 end
 
 # mongo.rb
+
