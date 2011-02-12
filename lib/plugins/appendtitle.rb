1b354ea41de1a608e78cc1281c05184d240bb5d0
hitode909 <hitode909@gmail.com>
2011-02-12 23:41:01 +0900

    appendtitle plugin expands short uri.

--- lib/plugins/appendtitle.rb
+++ lib/plugins/appendtitle.rb
@@ -9,25 +9,35 @@ module Termtter::Client
   config.plugins.appendtitle.set_default(:timeout, 30)
   config.plugins.appendtitle.set_default(:cache_expire, 3600 * 24 * 7)
 
-  def self.fetch_title(uri)
+  def self.fetch_title_data(uri) # returns {:title, :uri} | {:uri} | nil
     return unless uri
-    key = %w{ plugins appendtitle title}.push(Digest::SHA1.hexdigest(uri)).join('-')
+    key = %w{ plugins appendtitle title-data}.push(Digest::SHA1.hexdigest(uri)).join('-')
     if v = memory_cache.get(key)
       logger.debug "appendtitle: cache hit for #{uri}"
       return v
     end
 
-    memory_cache.set(key, '', config.plugins.appendtitle.cache_expire) # to avoid duplicate fetch
+    memory_cache.set(key, {}, config.plugins.appendtitle.cache_expire) # to avoid duplicate fetch
+    logger.debug "appendtitle: fetching title for #{uri}"
+    data = {}
     begin
-      logger.debug "appendtitle: fetching title for #{uri}"
-      source = Nokogiri(open(uri).read)
-      if source and source.at('title')
-        title = source.at('title').text
-        memory_cache.set(key, title, config.plugins.appendtitle.cache_expire)
-        return title
+      io = URI.parse(uri).read
+      base_uri = io.base_uri.to_s
+      base_uri = uri if base_uri.length > 1000
+      data[:uri] = base_uri
+      begin # title
+        source = Nokogiri(io)
+        title = source.at('title').text rescue nil
+        title ||= source.at('h1').text rescue nil
+        title ||= source.at('h2').text rescue nil
+        title.gsub(/\n/, '').gsub(/\s+/, ' ') if title
+        data[:title] = title if title
+      rescue
       end
-      nil
-     rescue Timeout::Error, StandardError
+      memory_cache.set(key, data, config.plugins.appendtitle.cache_expire)
+      data
+    rescue Timeout::Error, StandardError => error
+      logger.debug "appendtitle: error #{uri}, #{error.class.to_s}: #{error.message}"
       nil
     end
   end
@@ -39,20 +49,22 @@ module Termtter::Client
       threads = statuses.map do |status|
         Thread.new{
           begin
-          status.text.gsub!(URI.regexp(['http', 'https'])) {|uri|
-            title = fetch_title(uri)
-            title = title.gsub(/\n/, '').gsub(/\s+/, ' ') if title
+            status.text.gsub!(URI.regexp(['http', 'https'])) {|uri_before|
+            data  = fetch_title_data(uri_before) || {}
+            title = data[:title]
             body_for_compare = status.text.gsub(/\n/, '').gsub(/\s+/, ' ')
+            uri_after = data[:uri] || uri_before
             if title and not (
                 body_for_compare.include? title or
                 body_for_compare.include? title[0..(title.length/2)] or
                 body_for_compare.include? title[(title.length/2)..-1]) # XXX: heuristic!!!
-              "#{uri} (#{title})"
+              "#{uri_after} (#{title})"
             else
-              uri
+              uri_after
             end
             }
-          rescue
+          rescue => error
+            logger.debug "appendtitle: [ERROR] #{error.class.to_s}: #{error.message}"
           end
         }
       end
@@ -71,4 +83,4 @@ module Termtter::Client
 end
 
 # appendtitle.rb:
-# append title for uri.
+# append title for uri and expand short uri.
