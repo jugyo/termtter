ff56094956460d941e2b1437471471d82e5beddc
hitode909 <hitode909@gmail.com>
2011-02-12 22:53:39 +0900

    im.kayac notification plugin

---
+++ lib/plugins/im_kayac.rb
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+require 'im-kayac'
+require 'cgi'
+
+config.plugins.im_kayac.set_default(:user_name, nil)
+
+Termtter::Client.register_hook(
+  :name => :im_kayac,
+  :point => :output,
+  :exec => lambda { |statuses, event|
+    return unless event == :update_friends_timeline
+
+    user_name = config.plugins.im_kayac.user_name || config.user_name
+    replies = statuses.select{|s| s.text.include?(user_name) }
+    return if replies.empty?
+    replies.each{|s|
+      time = Time.parse(s.created_at).strftime(config.plugins.stdout.time_format_not_today)
+      text = CGI.unescapeHTML(s.text).gsub('&', '＆') # XXX
+      ImKayac.post(user_name, "#{s.user.screen_name}: #{text} #{time}")
+    }
+  })
+
+# im_kayac.rb - notify replies via im.kayac
+#
+# This plugin requires gem im-kayac.
+# This plugin does not support handler, password or sig.
