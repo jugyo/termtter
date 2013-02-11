677a46ec7b1d7773b2c1cdb2c146e2d91c994dc2
id774 <idnanashi@gmail.com>
2013-02-11 20:16:51 +0900

    #12 pause plugins is not exist

--- spec/plugins/pause_spec.rb
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-require File.expand_path(File.dirname(__FILE__)) + '/../spec_helper'
-describe Termtter do
-  it 'pause plugin' do
-    Termtter::Client.should_receive(:pause)
-    Termtter::Client.plug 'pause'
-  end
-end
