bc3b8191dd3c99123bf30c3f98c0c4710630e12a
id774 <idnanashi@gmail.com>
2013-02-11 21:28:59 +0900

    #13 Plug-in easy post works only when above 15 chars and enable confirm option

--- lib/plugins/easy_post.rb
+++ lib/plugins/easy_post.rb
@@ -1,5 +1,9 @@
 module Termtter::Client
   register_hook(:easy_post, :point => :command_not_found) do |text|
-    execute("update #{text}")
+    if config.confirm && text.length > 15
+      execute("update #{text}")
+    else
+      raise Termtter::CommandNotFound, text
+    end
   end
 end
