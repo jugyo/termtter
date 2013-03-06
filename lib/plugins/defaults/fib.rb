1d42e6b4877f42992d47939378d26c4dff5959d8
id774 <idnanashi@gmail.com>
2013-03-06 15:36:12 +0900

    Remove alias f of fib

--- lib/plugins/defaults/fib.rb
+++ lib/plugins/defaults/fib.rb
@@ -1,6 +1,5 @@
 def fib(n)i=0;j=1;n.times{j=i+i=j};i end
 Termtter::Client.register_command(:name => :fib,
-                                  :aliases => [:f, :ho],
                                   :exec => lambda do |arg|
   case arg
   when "ukumori"
