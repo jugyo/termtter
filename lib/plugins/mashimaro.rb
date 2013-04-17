64c4d3883ca4affdb8efae88b03b33fbec5985be
id774 <idnanashi@gmail.com>
2013-04-17 13:29:15 +0900

    Add mashimaro plugin

---
+++ lib/plugins/mashimaro.rb
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+module Termtter::Client
+  register_macro(:mashimaro_oyasumi, "update %s おやすみましまろ～ヽ(｡´･ω･`｡)ノ",
+    :help => ['mashimaro_oyasumi {SCREENNAME}', 'update "@{SCREENNAME} おやすみましまろ～ヽ(｡´･ω･`｡)ノ"']
+  )
+end
+
+module Termtter::Client
+  register_macro(:mashimaro_ohayo, "update %s おはようましまろ～ヽ(｡´･ω･`｡)ノ",
+    :help => ['mashimaro_ohayo {SCREENNAME}', 'update "@{SCREENNAME} おはようましまろ～ヽ(｡´･ω･`｡)ノ"']
+  )
+end
+
+module Termtter::Client
+  register_macro(:mashimaro_otsukare, "update %s おつかれましまろ～ヽ(｡´･ω･`｡)ノ",
+    :help => ['mashimaro_otsukare {SCREENNAME}', 'update "@{SCREENNAME} おつかれましまろ～ヽ(｡´･ω･`｡)ノ"']
+  )
+end
+
+module Termtter::Client
+  register_macro(:mashimaro_okaeri, "update %s おかえりましまろ～ヽ(｡´･ω･`｡)ノ",
+    :help => ['mashimaro_okaeri {SCREENNAME}', 'update "@{SCREENNAME} おかえりましまろ～ヽ(｡´･ω･`｡)ノ"']
+  )
+end
+
+# vim: fenc=utf8
