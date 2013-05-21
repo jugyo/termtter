3d403ee4a94dafce57ecefd4d7b1f32bb9ef6f40
ujihisa <ujihisa@gmail.com>
2013-05-21 11:25:05 -0700

    "user show" (a.k.a. "p" or "profile") didn't work due to old usage of rubytter

--- lib/plugins/defaults/user.rb
+++ lib/plugins/defaults/user.rb
@@ -5,7 +5,7 @@ module Termtter::Client
     help: ["user show USERNAME", "Show user's profile."]
   ) do |arg|
     user_name = arg.empty? ? config.user_name : arg
-    user = Termtter::API.twitter.user(user_name)
+    user = Termtter::API.twitter.user(screen_name: user_name)
     attrs = %w[ name screen_name url description profile_image_url location protected following
     friends_count followers_count statuses_count favourites_count
     id time_zone created_at utc_offset notifications
